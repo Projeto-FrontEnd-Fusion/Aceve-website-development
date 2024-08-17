6ef0660717a11791aaabf4629cac918b97bdb6c1
claudio silva <claudiosilva.one@gmail.com>
2024-08-17 19:21:11 -0300

    fix: add overflow to element to prevent content overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>
+      <body className="overflow-hidden">
         <Mainmenu />
         {children}
       </body>
