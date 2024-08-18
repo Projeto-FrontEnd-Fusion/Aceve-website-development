67e31c25c9d0c1cc6758f0f13c2f308e1b97762d
claudio silva <claudiosilva.one@gmail.com>
2024-08-17 23:44:59 -0300

    feat: configure rendering logic for menu items

--- src/components/Menu/mainmenu.tsx
+++ src/components/Menu/mainmenu.tsx
@@ -16,10 +16,10 @@ interface ImenuHeader extends ComponentProps<"nav"> {
   options?: string[];
 }
 const menuOptions: string[] = [
-  "Página Inicial",
+  "Inicio",
   "Quem Somos",
   "Contato",
-  "Nosso Brechó",
+  "Brechó",
   "Grupo Símios",
   "Doe agora",
 ];
@@ -41,48 +41,50 @@ export default function Mainmenu({
   const [isOpenMenu, setisOpenMenu] = useState<boolean>(false);
   return (
     <>
-      <nav {...props} className="overflow-hidden">
+      <nav
+        {...props}
+        className="overflow-hidden lg:flex lg:justify-between lg:w-full lg:pr-20 lg:h-[80px] lg:pl-20  lg:drop-shadow-2xl shadow lg:items-center"
+      >
         <header
           className={clsx(
-            "flex py-4 items-center border-b-2 drop-shadow-md h-14",
-            "mobileSmall:h-20",
-            // "xl:bg-red-500"
+            "flex py-4 items-center  h-14",
+            "mobileSmall:h-20 lg:flex-row "
           )}
         >
           <figure className="border-r-2 border-gray-stroker ml-4">
             <img
               src={headerDetails.logo}
               alt={headerDetails.description}
-              className={clsx("w-20 pr-2", "mobileSmall:w-24")}
+              className={clsx("w-20 pr-2", "mobileSmall:w-24", "lg:w-36")}
             />
             <figcaption className="sr-only">
               Esse é o logo da ong Violeta Eliz
             </figcaption>
           </figure>
+
           <h1
             className={clsx(
               "text-[10px] leading-none text-purple-light",
               "w-32 px-1",
               "mobileSmall:w-[45%] mobileSmall:px-12 mobileSmall:pl-2 mobileSmall:text-[12px] font-semibold",
               "md:text-[14px] md:w-[30%]",
-              "mobileLarger:w-[35%]"
+              "mobileLarger:w-[35%]",
+              "lg:w-[50%] lg:text-[12px]"
             )}
           >
             {headerDetails.description}
           </h1>
 
-          <section className="donate-button-menu hidden gap-2 w-[360px] justify-center md:flex  lg:w-[50%] lg:justify-end">
+          <section className="lg:hidden donate-button-menu hidden gap-2 w-[360px] justify-center md:flex  lg:w-[50%] lg:justify-end">
             <ButtonPrimarySmall btnType="secondary" title="Brechó" />
             <ButtonPrimarySmall btnType="primary" title="Doar Agora" />
-            {/* <figure>
-              <img src={headerDetails.logoSimios} alt="Logo Grupo simios" />
-            </figure> */}
           </section>
 
           <figure
             className={clsx(
               "flex items-center justify-end flex-1 mr-6 cursor-pointer",
-              "md:mr-4"
+              "md:mr-4",
+              "lg:hidden"
             )}
             onClick={() => setisOpenMenu(!isOpenMenu)}
           >
@@ -96,32 +98,53 @@ export default function Mainmenu({
 
         <ul
           className={clsx(
-            " overflow-hidden drop-shadow-r-lg h-screen w-screen flex flex-col items-center py-10 absolute transform transition-transform duration-500 ease-in-out bg-zinc-100",
+            " overflow-hidden drop-shadow-r-lg h-screen w-screen flex flex-col items-center py-10 absolute transform transition-transform duration-500 ease-in-out",
             {
-              "translate-x-0": isOpenMenu,
-              "translate-x-full border-l-[32px] border-purple-300": !isOpenMenu,
+              "translate-x-0 bg-zinc-100": isOpenMenu,
+              "drop-shadow-r-lg translate-x-full lg:translate-x-0 lg:border-none border-l-[32px] border-purple-300 lg:flex-row lg:relative h-screen lg:text-[12px] lg:w-auto lg:h-20 lg:bg-white":
+                !isOpenMenu,
             }
           )}
         >
           {menuOptions.map((items: string, index: number) => (
-            <Link
+            <li
               key={index}
-              href={items.trim().replace(/\s+/g, "-").toLocaleLowerCase()}
+              className={clsx(
+                "font-roboto lg:text-[16px]",
+
+                {
+                  "ordered-list hover:text-purple-light lg:hover:bg-white p-4 px-4 rounded-md transition-all ease-out duration-1000 lg:ordered-list":
+                    items !== menuOptions[5],
+                }
+              )}
             >
-              <li
-                key={index}
-                className={clsx("font-roboto", {
-                  "hover:text-purple-light hover:bg-zinc-200 p-4 px-4 rounded-md transition-all ease-out duration-1000":
-                    items !== "Doe Agora",
-                })}
+              <Link
+                href={items.trim().replace(/\s+/g, "-").toLocaleLowerCase()}
               >
-                {items !== "Doe agora" ? (
-                  items
+                {items == menuOptions[3] || items == menuOptions[5] ? (
+                  <ButtonPrimarySmall
+                    btnType={items == menuOptions[3] ? "secondary" : "primary"}
+                    title={items}
+                  />
                 ) : (
-                  <ButtonPrimarySmall btnType="primary" title={items} />
+                  <span
+                    className={clsx("flex", {
+                      "lg:hidden": items === "Grupo Símios",
+                    })}
+                  >
+                    {items}
+                  </span>
                 )}
-              </li>
-            </Link>
+
+                <figure
+                  className={clsx("hidden", {
+                    "lg:flex": items === "Grupo Símios",
+                  })}
+                >
+                  <img src={headerDetails.logoSimios} alt="logo" />
+                </figure>
+              </Link>
+            </li>
           ))}
         </ul>
       </nav>
