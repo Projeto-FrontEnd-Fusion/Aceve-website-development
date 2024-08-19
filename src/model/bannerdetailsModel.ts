3c9effddf2198b2dfa9ce7ba89ad787c6ceab02a
claudio silva <claudiosilva.one@gmail.com>
2024-08-19 11:39:26 -0300

    feat :  add banner details model

---
+++ src/model/bannerdetailsModel.ts
@@ -0,0 +1,8 @@
+export const bannerDetails = {
+  title: "Empoderamento e Igualdade",
+  bannerImg:
+    "https://files.edgestore.dev/nh2rz4kiwkl7w411/publicImages/_public/post/08114f59-755f-46b1-9786-6822a797885c.png",
+  description:
+    "É nosso dever criar oportunidades, quebrar barreiras do isolamento e não pertencimento de mulheres, para que elas possam desenvolver suas habilidades e ocupar o seu lugar de fala no mundo. Esse movimento visa promover a igualdade de gênero, raça e cidadania em nossa sociedade, além de combater todas as formas de violência contra as mulheres.",
+  action: "Doe Agora",
+};
