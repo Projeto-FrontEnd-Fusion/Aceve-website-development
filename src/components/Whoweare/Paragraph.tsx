2490361b57367983ec58b912c2bf6d26c63a4b5b
claudio silva <claudiosilva.one@gmail.com>
2024-08-23 13:19:52 -0300

    feat : Creating an image component for the about us session

---
+++ src/components/Whoweare/Paragraph.tsx
@@ -0,0 +1,37 @@
+import { whoweareDetails } from "@/model/whoweare";
+import clsx from "clsx";
+import { Button } from "../button/GlobalButton";
+import { FaArrowRight } from "react-icons/fa6";
+
+export const ParagraphSection = () => {
+  return (
+    <>
+      <article
+        className={clsx(
+          "py-12 px-6 font-roboto text-gray-normal flex flex-col gap-4",
+          "tablet:px-16",
+          "laptop:px-0 laptop:pr-20 laptop:py-0 laptop:gap-6"
+        )}
+      >
+        <p className="leading-6">{whoweareDetails.p1}</p>
+        <p className="leading-6">{whoweareDetails.p2}</p>
+        <p className="leading-6">{whoweareDetails.p3}</p>
+        <div
+          className={clsx(
+            "grid place-content-center transition-transform duration-1000",
+            "tablet:grid laptop:place-content-start tablet:mt-8"
+          )}
+        >
+          <Button
+            size="small"
+            backgroundColor="purple"
+            fontColor="white"
+            icon={<FaArrowRight />}
+          >
+            {whoweareDetails.action}
+          </Button>
+        </div>
+      </article>
+    </>
+  );
+};
--- src/components/Whoweare/whoweare.tsx
+++ src/components/Whoweare/whoweare.tsx
@@ -1,40 +1,23 @@
 import { whoweareDetails } from "@/model/whoweare";
 import { Button } from "../button/GlobalButton";
+import { FaArrowRight } from "react-icons/fa6";
+
 import clsx from "clsx";
+import { ImgSectionWhoWeAre } from "./ImgSection";
+import { HeaderSectionWhoweAre } from "./Header";
+import { ParagraphSection } from "./Paragraph";
 
 export const Whoweare = () => {
   return (
     <>
-      <section className=" flex-col gap-2 pb-10">
-        <h2 className="font-semibold text-[18px] font-roboto text-center py-6">
-          {whoweareDetails.title}
-        </h2>
-
-        <section className=" flex">
-          <article className=" flex justify-center items-center h-[200px] mobileMicro:h-[220px]">
-            <figure className={clsx("translate-x-4 z-10")}>
-              <img src={whoweareDetails.whoweareImg1} />
-            </figure>
-
-            <figure className={clsx("-translate-x-4 translate-y-8")}>
-              <img src={whoweareDetails.whoweareImg2} className="w-full" />
-            </figure>
-          </article>
-          <article
-            className={clsx(
-              "py-12 px-6 font-roboto text-gray-600 flex flex-col gap-4 ",
-              "tablet:bg-green-700"
-            )}
-          >
-            <p>{whoweareDetails.p1}</p>
-            <p>{whoweareDetails.p2}</p>
-            <p>{whoweareDetails.p3}</p>
-          </article>
-        </section>
-
-        <Button size="small" Background="purple" fontColor="white">
-          {whoweareDetails.action}
-        </Button>
+      <section
+        className={clsx(
+          "flex-col gap-2 pb-10",
+          "laptop:w-1/2 ml-auto laptop:relative laptop:pt-16 laptop:items-start"
+        )}
+      >
+        <HeaderSectionWhoweAre />
+        <ParagraphSection />
       </section>
     </>
   );
