029595a81fe2188b2d9e098f1403baea220040ec
claudio silva <claudiosilva.one@gmail.com>
2024-08-23 13:27:59 -0300

    feat : add desktop size

--- src/components/Whoweare/ImgSection.tsx
+++ src/components/Whoweare/ImgSection.tsx
@@ -6,15 +6,22 @@ export const ImgSectionWhoWeAre = () => {
     <div
       className={clsx(
         " flex justify-center items-center h-[300px]",
-        "laptop:absolute laptop:translate-y-[90px] laptop:-translate-x-[480px] transition-transform duration-1000"
+        "laptop:absolute laptop:translate-y-[90px] laptop:-translate-x-[480px] transition-transform duration-1000",
+        "desktop:laptop:-translate-x-[580px]"
       )}
     >
       <figure className={clsx("translate-x-4 z-10")}>
-        <img src={whoweareDetails.whoweareImg1} className="laptop:w-[220px]" />
+        <img
+          src={whoweareDetails.whoweareImg1}
+          className="laptop:w-[220px] desktop:w-[280px]"
+        />
       </figure>
 
       <figure className={clsx("-translate-x-4 translate-y-8")}>
-        <img src={whoweareDetails.whoweareImg2} className="laptop:w-[220px]" />
+        <img
+          src={whoweareDetails.whoweareImg2}
+          className="laptop:w-[220px] desktop:w-[280px]"
+        />
       </figure>
     </div>
   );
