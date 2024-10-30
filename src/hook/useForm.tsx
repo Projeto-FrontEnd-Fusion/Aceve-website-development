6d12930ee8e191eebc6df125c73b7244ed47cb17
Davi Santanna <davicalvalcanti@gmail.com>
2024-10-29 23:13:14 -0300

    FEAT: "add a hook for form validation"

---
+++ src/hook/useForm.tsx
@@ -0,0 +1,26 @@
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { SchemaDonationProps } from "@/model/schemas/schemaDonation";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface schemasOptions {
+  schemas: any;
+}
+
+export function useValidation(schemas: schemasOptions) {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SchemaDonationProps>({
+    resolver: yupResolver(schemas.schemas),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return { register, watch, handleSubmit: handleSubmit(onSubmit), errors };
+}
