72c6511db8e063150933e2f16bf61e346edd2692
Walassi Silva <walassi.b@gmail.com>
2024-11-06 23:20:01 -0300

    FEAT: add `HeroSection` component mobile version

--- src/components/Whoweare/Banner.tsx
+++ src/components/Whoweare/Banner.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const BannerWhoWeAre = () => {
   return (
     <div>
-      <img src="https:picsum.photos/id/342/1440/401" alt="" />
+      <img
+        src="https:picsum.photos/id/342/1440/1080"
+        alt="Grande grupo de pessoas juntas com os braços pro alto"
+        className="h-[216px] md:h-[400px] w-full object-cover"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default BannerWhoWeAre
+export default BannerWhoWeAre;
