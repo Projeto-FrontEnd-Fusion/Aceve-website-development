2b3a29a1732ff5ba7528ea3d222b58b7d05b0dff
Walassi Silva <walassi.b@gmail.com>
2024-11-07 23:24:17 -0300

    STYLE: add responsiveness to `WorkSection` component

--- src/components/whoweare-extended/WorksSection.tsx
+++ src/components/whoweare-extended/WorksSection.tsx
@@ -5,7 +5,9 @@ import { whoweareWorks as data } from "@/model/whoweare";
 const WorksSectionWhoWeAre = () => {
   return (
     <section className="bg-beige-normal font-roboto text-center pt-8">
-      <ul className="flex flex-col gap-6 px-5 ">
+      <ul className="flex flex-col gap-6 px-5 
+      tablet:flex-row lg:px-[5%] tablet:justify-center
+      desktop:px-[3%]">
         {data.map((work) => (
           <WorkItem key={work.title} work={work} />
         ))}
@@ -23,12 +25,14 @@ type WorkItemProps = {
 }
 const WorkItem = ({ work }: WorkItemProps) => {
   return (
-    <li className="flex flex-col items-center gap-4">
-      <div className="size-20 p-5 rounded-full bg-yellow-normal flex items-center justify-center">
+    <li className="flex flex-col items-center gap-4 
+    tablet:w-[308px]">
+      <div className="size-20 p-5 rounded-full bg-yellow-normal flex items-center justify-center
+      desktop:size-32">
         <img src={work.workImage} alt={work.title} className="" />
       </div>
       <h3 className="font-bold">{work.title}</h3>
-      <p className="px-5">{work.description}</p>
+      <p className="px-5 tablet:px-0">{work.description}</p>
     </li>
   );
 };
