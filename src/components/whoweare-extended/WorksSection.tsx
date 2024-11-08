adc8f56cd136d32c3d7ae02e999a8cf1102bca12
Walassi Silva <walassi.b@gmail.com>
2024-11-08 18:07:08 -0300

    STYLE: change `WorksSection` to grid layout

--- src/components/whoweare-extended/WorksSection.tsx
+++ src/components/whoweare-extended/WorksSection.tsx
@@ -4,11 +4,15 @@ import { whoweareWorks as data } from "@/model/whoweare";
 
 const WorksSectionWhoWeAre = () => {
   return (
-    <section className="bg-beige-normal font-roboto text-center py-8
-    tablet:py-20">
-      <ul className="flex flex-col gap-6 px-5 
-      tablet:flex-row lg:px-[5%] tablet:justify-center
-      desktop:px-[3%]">
+    <section
+      className="bg-beige-normal font-roboto text-center py-8
+    tablet:py-20"
+    >
+      <ul
+        className="grid grid-cols-1 gap-6 px-5 place-items-center
+          tablet:grid-cols-2 tablet:place-items-center tablet:px-[4%]
+          desktop:grid-cols-4"
+      >
         {data.map((work) => (
           <WorkItem key={work.title} work={work} />
         ))}
@@ -22,14 +26,18 @@ type WorkItemProps = {
     title: string;
     description: string;
     workImage: string;
-  }
-}
+  };
+};
 const WorkItem = ({ work }: WorkItemProps) => {
   return (
-    <li className="flex flex-col items-center gap-4 
-    tablet:w-[308px]">
-      <div className="size-20 p-5 rounded-full bg-yellow-normal flex items-center justify-center
-      desktop:size-32">
+    <li
+      className="flex flex-col items-center gap-4 p-2 max-w-[380px]
+        tablet:max-w-[308px]"
+    >
+      <div
+        className="size-20 p-5 rounded-full bg-yellow-normal flex items-center justify-center
+      desktop:size-32"
+      >
         <img src={work.workImage} alt={work.title} className="" />
       </div>
       <h3 className="font-bold">{work.title}</h3>
