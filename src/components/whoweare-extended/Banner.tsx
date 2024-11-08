8d23959510b0568fad928cef360793ad7e7b6c9b
Walassi Silva <walassi.b@gmail.com>
2024-11-08 18:59:04 -0300

    STYLE: ajust images size and positions

--- src/components/whoweare-extended/Banner.tsx
+++ src/components/whoweare-extended/Banner.tsx
@@ -4,7 +4,7 @@ const BannerWhoWeAre = () => {
   return (
     <div>
       <img
-        src="https:picsum.photos/id/342/1440/1080"
+        src="https://s3-alpha-sig.figma.com/img/f284/1b85/dc4ad913f77c6b2e7c5029c9b07a7c48?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M72vrMBh3JA6V43RRRpYDdGWHCktuCqdDlGtS3QketTgopZyZ8EBPmDaPkFxnhvs99wL~U29vWQJdhycJekJNfaOSzTQxmkavC-hpmKwKMqZxtJ4WjvErRCW6wCOdoGykRD1W6izDKK6OLvSMG32zEFTfhAMQdaz~ICmTHGJXe3I~rbPV1b3KNBluSmtjsDczwbU5PE9KDxC0YjZaQqeCXCedzGtHUCITijDO3S7AVjxV7udcgh-WdM8ifQsGyRqxhSB-399KJdsIhsIOP--boXlTCNYjNl50r1FDvWXdjUzM~G5o~ND79~9BaRJoSfQyWOz5Of8uaSbMySJJxPdiQ__"
         alt="Grande grupo de pessoas juntas com os braços pro alto"
         className="h-[216px] laptop:h-[400px] w-full object-cover"
       />
