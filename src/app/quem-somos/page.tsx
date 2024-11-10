f7b1f0f2910fd9363fcd126b6ab332615c9645e1
Walassi Silva <walassi.b@gmail.com>
2024-11-10 15:05:40 -0300

    FEAT: add finished `InfoSection` component

--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -1,9 +1,9 @@
+import React from "react";
 import BannerWhoWeAre from "@/components/whoweare-extended/Banner";
-import GallerySectionWhoWeAre from "@/components/whoweare-extended/GallerySection";
 import HeroSectionWhoWeAre from "@/components/whoweare-extended/HeroSection";
 import InfoSectionWhoWeAre from "@/components/whoweare-extended/InfoSection";
 import WorksSectionWhoWeAre from "@/components/whoweare-extended/WorksSection";
-import React from "react";
+import GallerySectionWhoWeAre from "@/components/whoweare-extended/GallerySection";
 
 const QuemSomos = () => {
   return (
