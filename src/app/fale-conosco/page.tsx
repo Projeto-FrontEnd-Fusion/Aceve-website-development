c0cd4ea1fe412f28a2edc5fa636ceb9d8a7b7c67
Matheus Amon <amonmatheus757@gmail.com>
2024-11-17 10:49:11 -0300

    FEAT: ensure social media section be responsive

--- src/app/fale-conosco/page.tsx
+++ src/app/fale-conosco/page.tsx
@@ -10,10 +10,10 @@ const TalkToUS = () => {
     return ( 
         <main>
             <section className="bg-beige-normal">
-                <div  className="flex flex-col items-center px-5 lg:px-[81px] py-8 lg:py-20 lg:w-[1280px] lg:mx-auto">
-                    <h1 className="uppercase font-roboto font-bold text-lg lg:text-[40px] mb-4">fale conosco</h1>
+                <div className="flex flex-col items-center px-5 lg:px-[81px] py-8 lg:py-20 w-[350px] lg:w-[1280px] mx-auto">
+                    <h1 className="uppercase font-roboto font-bold text-lg lg:text-[40px] lg:font-montserrat mb-4">fale conosco</h1>
                     <p className="font-roboto text-base text-[#444444] lg:text-lg mb-6 lg:mb-16">Compartilhe sua ideia, inspire a ação</p>
-                    <div className="flex flex-col items-center justify-between lg:gap-8 lg:flex-row w-full mt-4lg:mt-16">
+                    <div className="flex flex-row flex-wrap items-center justify-center lg:justify-between lg:gap-8 w-full mt-4lg:mt-16">
                         <div className="relative h-[193px] w-[262px] lg:min-w-[470px] lg:h-[547px]">
                             <Image
                                 fill
@@ -43,21 +43,23 @@ const TalkToUS = () => {
                 </div>
             </section>
 
-            <section className="flex flex-col items-center px-5 py-8 gap-4 bg-beige-normal">
-                <h1 className="uppercase font-roboto font-bold text-lg">acompanhe <span className="text-[#855CA3]">violeta eliz</span></h1>
+            <section className="bg-beige-normal">
+                <div className="flex flex-col items-center px-5 lg:px-[81px] py-8 lg:py-20 w-[350px] lg:w-[1280px] mx-auto">
+                    <h1 className="uppercase font-roboto font-bold text-lg lg:text-[40px] lg:font-montserrat">acompanhe <span className="text-[#855CA3]">violeta eliz</span></h1>
 
-                <div className="w-[212px] space-y-10">
-                    {
-                        socialMedia.map(socialMediaItem => (
-                            <SocialMediaItem
-                                key={socialMediaItem.socialMediaName}
-                                socialMediaName={socialMediaItem.socialMediaName}
-                                text={socialMediaItem.text}
-                                socialMediaLink={socialMediaItem.socialMediaLink}
-                                iconUrl={socialMediaItem.iconUrl}
-                            />
-                        ))
-                    }           
+                    <div className="flex flex-row flex-wrap justify-center gap-10 lg:gap-[97px] mt-8 lg:mt-16 transition-all duration-500">
+                        {
+                            socialMedia.map(socialMediaItem => (
+                                <SocialMediaItem
+                                    key={socialMediaItem.socialMediaName}
+                                    socialMediaName={socialMediaItem.socialMediaName}
+                                    text={socialMediaItem.text}
+                                    socialMediaLink={socialMediaItem.socialMediaLink}
+                                    iconUrl={socialMediaItem.iconUrl}
+                                />
+                            ))
+                        }           
+                    </div>
                 </div>
             </section>
         </main>
