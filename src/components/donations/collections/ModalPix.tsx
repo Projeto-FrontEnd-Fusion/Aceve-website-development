4dd2e0f216f1725bb2d5bdafa948b8c2ab213896
Elen Damares <elen.damares774@gmail.com>
2025-06-05 22:04:45 -0300

    feat: add a modal holder with a black background

--- src/components/donations/collections/ModalPix.tsx
+++ src/components/donations/collections/ModalPix.tsx
@@ -2,9 +2,13 @@ interface modalPixProps {
   closeModal: ()=> void;
 }
 export const ModalPix = ({ closeModal } : modalPixProps) =>{
-  return(<>
-  Modal
-  <button 
-  type="button"
-  onClick={closeModal}>Fechar</button></>)
+  return(
+  <div className="modal-holder w-full h-full bg-[#00000091] absolute z-30 top-0
+  flex flex-col items-center justify-start">
+    <div className="modal-body bg-[white] w-[90vw] md:w-[600px] h-[600px] rounded-sm mt-10">
+      <button 
+      type="button"
+      onClick={closeModal}>Fechar</button>
+    </div>
+  </div>)
 }
