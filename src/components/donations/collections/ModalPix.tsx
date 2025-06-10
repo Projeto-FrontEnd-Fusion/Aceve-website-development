d9e560f561e06a2041e10b17c018bc15bdd9ec6b
Elen Damares <elen.damares774@gmail.com>
2025-06-10 18:53:15 -0300

    feat: enable ESC key to close modal

--- src/components/donations/collections/ModalPix.tsx
+++ src/components/donations/collections/ModalPix.tsx
@@ -2,13 +2,34 @@
 import Image from 'next/image'
 import pixQrCode from '@/assets/pixQrCode.jpg'
 import { IoClose } from "react-icons/io5";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface modalPixProps {
   closeModal: ()=> void;
 }
 export const ModalPix = ({ closeModal } : modalPixProps) =>{
   const [copiedText, setCopiedText] = useState(false)
+  const modalRef = useRef<null | HTMLDivElement>(null)
+
+  useEffect(()=>{
+    function handleKeydown(e: KeyboardEvent){
+      if(e.key == 'Escape'){
+        closeModal()
+      }
+    }
+
+    document.addEventListener('keydown', handleKeydown)
+
+    return ()=>{
+      document.removeEventListener('keydown', handleKeydown)
+    }
+  },[closeModal])
+
+  useEffect(()=>{
+    if(modalRef.current){
+      const focusableElements = modalRef.current.querySelectorAll('button, .qr-code,')
+    }
+  },[modalRef])
 
   async function handleCopyText(){
     await navigator.clipboard.writeText("11964771181")
@@ -21,7 +42,7 @@ export const ModalPix = ({ closeModal } : modalPixProps) =>{
   }
   
   return(
-  <div className="modal-holder w-full h-full bg-[#00000091] absolute z-30 top-0
+  <dialog className="modal-holder w-full h-full bg-[#00000091] absolute z-30 top-0
   flex flex-col items-center justify-start">
     <div className="modal-body bg-[white] w-[90vw] md:w-[600px] h-fit rounded-lg mt-10 flex flex-col items-center justify-center relative">
         <button 
@@ -29,15 +50,16 @@ export const ModalPix = ({ closeModal } : modalPixProps) =>{
         onClick={closeModal}
         aria-label="Fechar modal"
         title="Fechar modal"
-        className='absolute top-2 right-2 flex flex-row items-center justify-center'>
+        className='absolute top-2 right-2 flex flex-row items-center justify-center'
+        tabIndex={1}>
         <IoClose size={'2em'} className="hover:fill-red-500 transition-colors duration-300"/>
-      </button>
+        </button>
       <h4 className='text-[2em]'>Pix</h4>
       <Image
       src={pixQrCode}
       width={600}
       height={600}
-      className='w-[90%] h-[auto] md:w-[300px] md:h-[300px]'
+      className='qr-code w-[90%] h-[auto] md:w-[300px] md:h-[300px]'
       alt="QR Code do pix para doações"/>
       <div className='chave-pix grid grid-cols-1 md:grid-cols-2 grid-rows-[auto_auto_1.5em] md:grid-rows-2 gap-x-2 justify-center mt-2 mb-1'>
         <span className='flex flex-col items-center w-full'>
@@ -58,5 +80,5 @@ export const ModalPix = ({ closeModal } : modalPixProps) =>{
         )}
       </div>
     </div>
-  </div>)
+  </dialog>)
 }
