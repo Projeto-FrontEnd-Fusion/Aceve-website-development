23b5948bce1646304eec8a758e390f81a779817d
Elen Damares <elen.damares774@gmail.com>
2025-06-16 14:00:05 -0300

    change bg and remove gradient

--- src/components/Banner/mainbanner.tsx
+++ src/components/Banner/mainbanner.tsx
@@ -6,7 +6,6 @@ import clsx from "clsx";
 import { toggleMenuStore } from "@/zustand-store/togglemenu.store";
 import { useStore } from "zustand";
 import { BiDonateHeart } from "react-icons/bi";
-import { useEffect, useState } from "react";
 
 const BannerHero = ({
   title,
@@ -32,55 +31,35 @@ const BannerHero = ({
           alt="Imagem de um banner representando empoderamento e igualdade"
           className="w-full border-slate-950"
         />
-        <div
-          className={clsx(
-            "absolute",
-            "inset-x-0",
-            "bottom-[-2px]",
-            "h-1/2",
-            "bg-gradient-to-t",
-            "from-purple-haiti",
-            "to-transparent",
-            "desktop:h-auto",
-            "desktop:w-1/2",
-            "desktop:inset-y-0",
-            "desktop:left-0",
-            "desktop:bg-gradient-to-r",
-            "desktop:from-purple-haiti",
-            "desktop:to-transparent"
-          )}
-        ></div>
         <figcaption className="sr-only">
           A imagem retrata uma mulher negra em meio a uma comunidade carente de
           uma cidade brasileira. Ela exibe uma expressão determinada, com o
           rosto sério, enquanto levanta uma mão com o punho cerrado,
           simbolizando força e resistência.
         </figcaption>
-        <div className="shadeContainer"></div>
       </figure>
 
       <article
         className={clsx(
-          "w-screen bg-purple-haiti px-7 flex flex-col gap-4 mt-[-8px] py-10 items-center",
+          "w-screen px-7 flex flex-col gap-4 mt-[-8px] py-10 items-center",
           "desktop:justify-center desktop:items-start desktop:pl-24 desktop:w-[80%]"
         )}
       >
         <h2
           className={clsx(
-            "text-white-normal font-montserrat font-bold mb-4 text-[20px] leading-7 ",
+            "text-black font-montserrat font-bold mb-4 text-[20px] leading-7 ",
             " mobileMicro:pr-8",
             "mobileXLarger:w-full mobileXLarger:pr-[50%] mobileXLarger:leading-8",
             "laptop:pr-[60%] laptop:text-[32px]",
             "mobileMini:text-[28px] desktop:text-[36px] desktop:leading-[36px]",
             "desktopLarge:text-[56px] desktopLarge:pr-[40%] desktopLarge:leading-[56px]"
           )}
-          // consertei o texto desproporcional em telas entre 412px e 480px
         >
           {title}
         </h2>
         <p
           className={clsx(
-            "w-full text-white-normal font-roboto text-[14px] mb-8 leading-6",
+            "w-full text-black font-roboto text-[14px] mb-8 leading-6",
             "mobileMicro:text-[12px]",
             "mobileXLarger:text-[16px]",
             "text-[18px] desktop:text-[18px] desktop:mb-10 desktop:w-full desktop:leading-7"
