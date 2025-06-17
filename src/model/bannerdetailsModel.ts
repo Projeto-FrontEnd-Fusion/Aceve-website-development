2e3f0ae6b6df544346f4919a942febe499ad1de2
Dalila Castro <dalilaccastro@gmail.com>
2025-06-17 20:12:30 -0300

    feat: update banner text to gray version and restyle button changing appearance and hover

--- src/model/bannerdetailsModel.ts
+++ src/model/bannerdetailsModel.ts
@@ -1,7 +1,7 @@
 export const bannerDetails = {
-  title: "Empoderamento e Igualdade",
+  //title: "Empoderamento e Igualdade",
   bannerImg: "https://i.imgur.com/NE6EVuK.png",
   description:
-    "É nosso dever criar oportunidades, quebrar barreiras do isolamento e não pertencimento de mulheres, para que elas possam desenvolver suas habilidades e ocupar o seu lugar de fala no mundo. Esse movimento visa promover a igualdade de gênero, raça e cidadania em nossa sociedade, além de combater todas as formas de violência contra as mulheres.",
-  action: "Doe Agora",
+    "Capacitamos mulheres em situação de vulnerabilidade para que conquistem autonomia, rompam ciclos de violência e transformem suas vidas.",
+  action: "AJUDE AGORA",
 };
