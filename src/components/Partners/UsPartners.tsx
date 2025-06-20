9c2f075140f5a384c597ee88b9021b67797b620d
Davi Santanna <davicalvalcanti@gmail.com>
2025-06-20 14:29:48 -0300

    refactor: replace partner logos URLs with local images and update layout

--- src/components/Partners/UsPartners.tsx
+++ src/components/Partners/UsPartners.tsx
@@ -1,30 +1,31 @@
+import Image from "next/image";
 import { HeaderSection } from "../Header";
+import LogoCeA from "@/public/InstitutoCeA.png";
+import LogoRemimo from "@/public/RemimoEcojoias.png";
+import LogoBrecholeta from "@/public/BrecholetaVioletaEliz.png";
 
 export const UsPartners = () => {
   const patnesImg = [
     {
-      name: "instituto c&a",
-      icon: "https://i.imgur.com/TCPvBub.png",
+      name: "Instituto c&a",
+      icon: LogoCeA,
     },
     {
-      name: "remimo",
-      icon: "https://i.imgur.com/jDwCHNo.png",
+      name: "Remimo",
+      icon: LogoRemimo,
     },
     {
-      name: "brecholeta",
-      icon: "https://i.imgur.com/tDEB4yF.png",
+      name: "Brecholeta",
+      icon: LogoBrecholeta,
     },
   ];
   return (
-    <section className="py-6 flex-col desktop:w-1/2 mx-auto desktop:py-20">
-      <HeaderSection
-        title="PARCEIROS"
-        sub="Conheça os parceiros que caminham conosco: juntos, ampliamos horizontes e fortalecemos nossa missão"
-      />
-      <div className="flex justify-center gap-8 items-center my-8">
+    <section className="py-12 flex flex-col gap-8">
+      <HeaderSection title="PARCEIROS" />
+      <div className="flex justify-center gap-8 items-center max-mobileLarger:flex-col md:gap-20">
         {patnesImg.map(({ name, icon }) => (
           <figure>
-            <img src={icon} alt={name} />
+            <Image src={icon} className="max-w-30" alt={`Logo ${name}`} />
           </figure>
         ))}
       </div>
