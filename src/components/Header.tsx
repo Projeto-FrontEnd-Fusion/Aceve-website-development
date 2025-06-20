6a9badce7c7d4e2edc94523582eb8752716df531
Davi Santanna <davicalvalcanti@gmail.com>
2025-06-20 14:30:31 -0300

    refactor: simplify HeaderSection by removing subtitle and updating styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,12 @@
 interface HeaderSectionProps {
   title: string;
-  sub: string;
 }
-export const HeaderSection = ({ title, sub }: HeaderSectionProps) => {
+export const HeaderSection = ({ title }: HeaderSectionProps) => {
   return (
-    
-    
     <header className="text-center font-montserrat">
-      <h2
-        className="text-[1.2rem] font-bold text-preto-bunker 
-    desktop:text-[1.6rem]
-    "
-      >
+      <h2 className="max-mobileLarger:text-lg text-3xl font-bold text-[#54287B]">
         {title}
       </h2>
-    
-      <h3
-        className="text-[.8rem] font-normal 
-    desktop:text-[.9rem] px-6
-    "
-      >
-        {sub}
-      </h3>
-      
     </header>
-    
-  
-    
   );
 };
