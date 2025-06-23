cfc0f9ea2f8144734fd1735641e91b674cf7cce5
Elen Damares <elen.damares774@gmail.com>
2025-06-22 22:33:31 -0300

    refactor: remove component props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,11 @@ import { FooterBanner } from "@/components/FooterBanner/Banner";
 import { ImpactHistory } from "@/components/History/ImpactHistory";
 import { UsPartners } from "@/components/Partners/UsPartners";
 import Partners from "@/components/whoweare-extended/partners-section";
-import { bannerDetails } from "@/model/bannerdetailsModel";
 
 export default function Home() {
   return (
     <main className="overflow-x-hidden">
-      <BannerHero
-        title={bannerDetails.title}
-        description={bannerDetails.description}
-        bannerImg={bannerDetails.bannerImg}
-      />
+      <BannerHero/>
      {/*  <ImpactHistory /> */}
 {/*       <FooterBanner /> */}
       <UsPartners />
