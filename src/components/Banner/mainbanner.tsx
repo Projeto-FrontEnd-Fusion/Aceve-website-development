63727c8b65f6f3437a68367dfd9f99c566fba118
Elen Damares <elen.damares774@gmail.com>
2025-06-22 22:33:31 -0300

    refactor: remove component props

--- src/components/Banner/mainbanner.tsx
+++ src/components/Banner/mainbanner.tsx
@@ -1,5 +1,4 @@
 "use client";
-import type { IbannerProps } from "@/@types/bannerTypes";
 import { Button } from "../button/GlobalButton";
 import Link from "next/link";
 import clsx from "clsx";
@@ -8,18 +7,11 @@ import { useStore } from "zustand";
 import Image from "next/image";
 import BannerImg from '@/public/BannerImg.jpg'
 
-const BannerHero = ({
-  title,
-  description,
-  action,
-  bannerImg,
-  ...props
-}: IbannerProps) => {
+const BannerHero = () => {
   const { isOpenMenu } = useStore(toggleMenuStore);
 
   return (
     <section
-      {...props}
       className={clsx(
         "w-full  flex flex-col relative ",
         "desktop:overflow-y-hidden desktop:flex-row-reverse",
@@ -42,7 +34,7 @@ const BannerHero = ({
 
       <article
         className={clsx(
-          "w-screen px-7 flex flex-col gap-4 mt-[-8px] py-10 items-center",
+          "w-screen px-7 flex flex-col gap-1 mt-[-8px] py-10 items-center",
           "desktop:justify-center desktop:items-start desktop:pl-[2.5rem] desktop:w-[80%]"
         )}
       >
@@ -76,14 +68,14 @@ const BannerHero = ({
             "text-[18px] desktop:text-[18px] desktop:mb-10 desktop:w-full desktop:leading-7"
           )}
         >
-          {description}
+          Capacitamos mulheres em situação de vulnerabilidade para que conquistem autonomia, rompam ciclos de violência e transformem suas vidas.
         </p>
 
         <Button
           Background="purple"
           size="small"
           fontColor="white"
-          className="bg-[#823DC7] hover:bg-[#663198] rounded-md transition-colors px-6 py-2"
+          className="bg-[#823DC7] hover:bg-[#663198] font-semibold rounded-md transition-colors px-6 py-3"
         >
           <Link href={"/doe-agora"}>AJUDE AGORA</Link>
         </Button>
--- src/model/bannerdetailsModel.ts
+++
@@ -1,7 +0,0 @@
-export const bannerDetails = {
-  //title: "Empoderamento e Igualdade",
-  bannerImg: "https://i.imgur.com/NE6EVuK.png",
-  description:
-    "Capacitamos mulheres em situação de vulnerabilidade para que conquistem autonomia, rompam ciclos de violência e transformem suas vidas.",
-  action: "AJUDE AGORA",
-};
