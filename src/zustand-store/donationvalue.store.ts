e9e9d1b41f3ac64c7dd000438543f37cd019acfe
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-07-14 03:36:43 -0300

    fix: add `null`  for zustand store setter type defitinion

--- src/zustand-store/donationvalue.store.ts
+++ src/zustand-store/donationvalue.store.ts
@@ -3,10 +3,10 @@ import { create } from 'zustand';
 
 interface IDonationStore {
   donationValue: number | null;
-  setDonationValue: (value: number) => void;
+  setDonationValue: (value: number | null) => void;
 }
 
 export const useDonationStore = create<IDonationStore>((set) => ({
   donationValue: null,
-  setDonationValue: (value: number) => set((state) => ({ donationValue: value })),
+  setDonationValue: (value: number | null) => set((state) => ({ donationValue: value })),
 }));
