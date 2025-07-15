58746a9e8a6a0a4e185ddf56b5ccfe10e5824e48
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-07-15 15:03:22 -0300

    feat: add heading and main description

--- src/app/quero-doar/page.tsx
+++ src/app/quero-doar/page.tsx
@@ -41,10 +41,19 @@ function QueroDoar() {
   return (
     <main
       className="flex flex-col min-h-screen bg-[#FAF6FE] items-center py-8
-      px-4 mobileMicro:px-8 mobileMini:px-16 mobileSmall:px-20 mobileLarger:px-30 mobileXLarger:px-36 tablet:px-8 laptop:px-32 desktop:px-0
-    "
+      px-4 mobileMicro:px-8 mobileMini:px-16 mobileSmall:px-20 mobileLarger:px-30 mobileXLarger:px-36 tablet:px-8 laptop:px-32 desktop:px-[72px]"
     >
-      <div className="flex flex-col w-fit tablet:w-full tablet:gap-8 items-center">
+      <div className="flex flex-col tablet:w-full tablet:gap-8 items-center">
+
+        <div className='flex flex-col w-full items-center tablet:items-start'>
+          <h1 className="text-[40px] text-[#663198] text-xl font-bold tablet:text-[40px]">
+            Doações e Arrecadações
+          </h1>
+          <p className="text-lxl text-[#5F5764] mt-2  pb-8 tablet:pt-5">
+            Sua doação financia ações sociais que acolhem, cuidam e empoderam mulheres, crianças e jovens em situação de vulnerabilidade nas favelas da Zona Sul de São Paulo.
+          </p>
+        </div>
+
         <section
           className="flex py-6 flex-col items-center gap-5
         w-full desktop:w-[60%]
@@ -56,17 +65,17 @@ function QueroDoar() {
           </h2>
 
           <ul
-            className="w-full gap-6 flex flex-col 
+            className="w-full gap-6 flex flex-col pb-5
         tablet:px-8 tablet:py-4
         tablet:grid tablet:grid-rows-2 tablet:grid-cols-2"
           >
             {impactList.map(({ about, icon, subTittle }, index) => (
-              <li key={index + 1} className="w-fit flex flex-col gap-1 mx-auto">
+              <li key={index + 1} className="w-fit flex flex-col gap-1 tablet:mx-auto">
                 <h3 className="flex font-semibold gap-2 text-[#5F5764] text-xs leading-5 items-center  min-[430px]:text-sm md:text-[14px] md:text-[#A5A1A8]">
                   {icon}
                   {subTittle}
                 </h3>
-                <p className="w-full text-[#1E1E1F] text-xs leading-5 min-[430px]:text-sm max-w-80 ">
+                <p className="w-full text-[#1E1E1F] text-xs leading-5 min-[430px]:text-sm">
                   {about}
                 </p>
               </li>
@@ -74,7 +83,7 @@ function QueroDoar() {
           </ul>
         </section>
         <section
-          className="flex flex-col items-center justify-center w-full gap-[40px]  
+          className="flex flex-col items-center justify-center w-full gap-5 tablet:gap-10 
         bg-[#FAF6FE] tablet:bg-[#fff] tablet:rounded-[16px] 
         tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
         tablet:w-full desktop:w-[60%]
