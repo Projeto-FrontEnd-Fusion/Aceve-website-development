1781f59b61f6fc69f172970e832fab9b113e7b39
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2025-07-17 10:34:12 -0300

    feat: page back button logic applyied

--- src/components/BackButton/backButton.tsx
+++ src/components/BackButton/backButton.tsx
@@ -1,16 +1,17 @@
-import Link from "next/link"
+"use client"
+import { useRouter } from "next/navigation";
 
 export const BackButton = () => {
-    return (
-         <div className="relative flex flex-col items-center justify-center w-full pt-8 
+  const router = useRouter();
+
+  return (
+    <div className="relative flex flex-col items-center justify-center w-full pt-8 
         tablet:rounded-[16px] tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
         tablet:w-[85%] ">
 
-          <Link href="/">
-            <button className="absolute top-0 left-0 text-[#A5A1A8] text-xl font-semibold font-inter transition">
-             &lt;    Voltar
-            </button>
-          </Link>
-        </div>
-    )
+      <button className="absolute top-0 left-0 text-[#A5A1A8] text-xl font-semibold font-inter transition" onClick={() => router.back()}>
+        &lt;    Voltar
+      </button>
+    </div>
+  )
 }
