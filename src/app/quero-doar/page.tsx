1ee416c232e9256193cb24bdd362dd0ca8301b37
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2025-07-17 10:36:59 -0300

    fix: wrongly placed the back button on quero-doar page

--- src/app/quero-doar/page.tsx
+++ src/app/quero-doar/page.tsx
@@ -1,24 +1,18 @@
-import { BackButton } from "@/components/BackButton/backButton";
 import { DonationOptions } from "@/components/DonationOptions/DonationOptions";
 
 function QueroDoar() {
   return (
     <main className="flex flex-col min-h-screen bg-[#FAF6FE] items-center">
-      <h1 className="sr-only">Quero Doar</h1>
+      <h1>Quero Doar</h1>
 
-      <section className="flex flex-col items-center justify-center w-full my-20 
-        tablet:rounded-[16px] tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
-        tablet:w-[85%]">
-        <BackButton />
-        <div className="flex flex-col items-center justify-center w-full gap-[24px] bg-[#FFF] 
-        tablet:rounded-[16px] tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
-        tablet:w-[85%] shadow-[1px_1px_4px_rgba(52,52,52,0.2)]">
-
-          <h2 className="text-[24px] text-[#663198] font-semibold">
-            Escolha o valor da sua doação
-          </h2>
-          <DonationOptions />
-        </div>
+      <section className='flex flex-col items-center justify-center w-full gap-[40px]  
+        bg-[#FAF6FE] tablet:bg-[#fff] tablet:rounded-[16px] 
+        tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
+        tablet:w-[85%] desktop:w-[60%]'>
+        <h2 className="text-[24px] text-[#663198] font-semibold">
+          Escolha o valor da sua doação
+        </h2>
+        <DonationOptions />
       </section>
     </main>
   );
