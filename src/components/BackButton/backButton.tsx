3d2942a6f5bfefce6005dee3ae33c34f50d17e57
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2025-07-18 13:50:23 -0300

    fix: ajustments on back page button style and placement

--- src/components/BackButton/backButton.tsx
+++ src/components/BackButton/backButton.tsx
@@ -5,13 +5,16 @@ export const BackButton = () => {
   const router = useRouter();
 
   return (
-    <div className="relative flex flex-col items-center justify-center w-full pt-8 
+    <div className="relative flex flex-col items-center justify-center w-full left-6 py-6 
         tablet:rounded-[16px] tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
         tablet:w-[85%] ">
 
-      <button className="absolute top-0 left-0 text-[#A5A1A8] text-xl font-semibold font-inter transition" onClick={() => router.back()}>
-        &lt;    Voltar
-      </button>
+      <div className="flex w-full absolute top-0 left-4 text-[#A5A1A8] text-xl font-semibold font-inter transition">
+        <button className="flex justify-between items-center gap-4 hover:cursor-pointer" onClick={() => router.back()}>
+          <span>&lt;</span>
+          Voltar
+        </button>
+      </div>
     </div>
   )
 }
