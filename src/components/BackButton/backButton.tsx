55900f0484121b222abb59419939f25bd9e59b5a
Dalila Castro <dalilaccastro@gmail.com>
2025-07-18 23:18:06 -0300

    fix: updated payment method page behavior and responsiveness, adjusted back button positioning and hover

--- src/components/BackButton/backButton.tsx
+++ src/components/BackButton/backButton.tsx
@@ -1,20 +1,16 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 
 export const BackButton = () => {
   const router = useRouter();
 
   return (
-    <div className="relative flex flex-col items-center justify-center w-full left-6 py-6 
-        tablet:rounded-[16px] tablet:py-[16px] tablet:px-[32px] desktop:py-[24px] 
-        tablet:w-[85%] ">
-
-      <div className="flex w-full absolute top-0 left-4 text-[#A5A1A8] text-xl font-semibold font-inter transition">
-        <button className="flex justify-between items-center gap-4 hover:cursor-pointer" onClick={() => router.back()}>
-          <span>&lt;</span>
-          Voltar
-        </button>
-      </div>
-    </div>
-  )
-}
+    <button
+      onClick={() => router.back()}
+      className="flex items-center gap-2 text-[#A5A1A8] text-lg font-semibold rounded-[8px] hover:bg-[#C1C2C7] active:bg-[#D3D4D9] px-5 py-4 transition mb-0"
+    >
+      <span className="text-lg">&lt;</span>
+      Voltar
+    </button>
+  );
+};
