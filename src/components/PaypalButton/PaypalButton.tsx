f86b6abab91c8df41e7a847d9d38b71b132b54cd
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-07-20 12:50:51 -0300

    fix: adjust button size with a fake div to match existing button style

--- src/components/PaypalButton/PaypalButton.tsx
+++ src/components/PaypalButton/PaypalButton.tsx
@@ -26,10 +26,10 @@ export const PaypalButton = () => {
             color: "silver",
             label: "paypal",
           }}
+          className="flex h-[56px] bg-[#EEEEEE] hover:bg-[#E2E2E2] rounded-md justify-start items-center"
           createOrder={createOrder(donationValue)}
           onApprove={captureOrder} />
       }
     </PayPalScriptProvider>
-
   )
 }
