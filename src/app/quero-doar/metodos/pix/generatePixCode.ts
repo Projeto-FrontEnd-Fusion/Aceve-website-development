ad169cf58ae6e08538d33eaa7b0d61121f53eb9d
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-07-21 14:09:53 -0300

    style: automatic indentation

--- src/app/quero-doar/metodos/pix/generatePixCode.ts
+++ src/app/quero-doar/metodos/pix/generatePixCode.ts
@@ -1,4 +1,4 @@
-import { createStaticPix} from 'pix-utils';
+import { createStaticPix } from 'pix-utils';
 import type { PixStaticObject } from 'pix-utils';
 import { off } from 'process';
 
@@ -6,31 +6,32 @@ export const generatePixCode = async (amount: number) => {
 
   console.log("Chamando generatePixCode com valor:", amount);
 
-  
+
   if (!amount || amount <= 0) {
     console.error('Valor inválido para doação.');
-    return { qrCodeBase64: null, brCode: null, error: "Valor inválido para doação." }; 
-  
+    return { qrCodeBase64: null, brCode: null, error: "Valor inválido para doação." };
+
   }
 
-  try{ const pixResult = await createStaticPix({
-    merchantName: 'Elizangela M. dos Santos',
-    merchantCity: 'Sao Paulo',
-    pixKey: '+5511964771181',
-    infoAdicional: '',
-    transactionAmount: amount,
-  });
+  try {
+    const pixResult = await createStaticPix({
+      merchantName: 'Elizangela M. dos Santos',
+      merchantCity: 'Sao Paulo',
+      pixKey: '+5511964771181',
+      infoAdicional: '',
+      transactionAmount: amount,
+    });
 
-  //throw new Error ("Erro Simulado ao gerar qr code")
-  
-  pixResult.throwIfError();
+    //throw new Error ("Erro Simulado ao gerar qr code")
 
-  const validPix= pixResult as PixStaticObject;
+    pixResult.throwIfError();
 
-  const qrCodeBase64 = await validPix.toImage();
-  const brCode = await validPix.toBRCode();
+    const validPix = pixResult as PixStaticObject;
 
-  return { qrCodeBase64, brCode, error: null };
+    const qrCodeBase64 = await validPix.toImage();
+    const brCode = await validPix.toBRCode();
+
+    return { qrCodeBase64, brCode, error: null };
 
   } catch (err: any) {
     console.error("Erro ao gerar código PIX:", err.message || err);
