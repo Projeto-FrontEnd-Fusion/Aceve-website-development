63a11a1b2352953c92aa4633428e9c6bbbb04c8d
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-07-28 02:09:28 -0300

    fix: iframe responsive design

--- src/features/home/components/Media/Media.tsx
+++ src/features/home/components/Media/Media.tsx
@@ -10,9 +10,9 @@ export function Media() {
       <h3 className="text-lg leading-6 text-primary-800 tracking-tighter font-semibold mb-4 min-[400px]:text-3xl ">
         Nossa causa em destaque
       </h3>
-      <div className="flex flex-col gap-8 md:flex-row">
+      <div className="flex w-full flex-col gap-8 md:flex-row">
         <iframe
-          className="w-full aspect-video rounded"
+          className="flex-1 min-w-0 aspect-video rounded"
           src="https://www.youtube.com/embed/oZbtMHtT_LI"
           title="Tribunal de Justiça de SP registra recorde de medidas protetivas concedidas em 2024"
           frameBorder="0"
@@ -22,7 +22,7 @@ export function Media() {
         ></iframe>
 
         <iframe
-          className="w-full aspect-video rounded"
+          className="flex-1 min-w-0 aspect-video rounded"
           src="https://www.youtube.com/embed/rbPbmunqQro"
           title="2°EPISODIO DO PODCAST LIBERTÁRIOS DO CAPÃO."
           frameBorder="0"
