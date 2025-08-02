be889bd2524bf6de3089877eeef2d31b908220ea
elen <elenndev@gmail.com>
2025-08-02 16:27:40 -0300

    feat: add text line/font style

--- src/features/about-us/components/ImpactfulStories.tsx
+++ src/features/about-us/components/ImpactfulStories.tsx
@@ -4,11 +4,15 @@ import ImpactfulStory3 from "@/assets/impactful-stories-3.png";
 import Image from "next/image";
 
 export const ImpactfulStories = () => {
+
+  const paragraphClassname = "text-sm tablet:text-base desktop:text-xl"
   const impactfulStories = [
     {
       title: "De Mãos que Cuidam a um Negócio que Transforma",
       paragraph:
-        <p>Com o apoio da ONG, a ex-técnica de enfermagem <span className="font-bold">Jucelia Pereira</span>, 44, fundou uma bem-sucedida loja de cosméticos. Sua missão, no entanto, vai além do negócio: como voluntária, ela agora dedica seu tempo para inspirar novas empreendedoras, provando que é possível transformar a própria história.</p>,
+        <p className={paragraphClassname}>
+          Com o apoio da ONG, a ex-técnica de enfermagem <span className="font-bold">Jucelia Pereira</span>, 44, fundou uma bem-sucedida loja de cosméticos. Sua missão, no entanto, vai além do negócio: como voluntária, ela agora dedica seu tempo para inspirar novas empreendedoras, provando que é possível transformar a própria história.
+        </p>,
       image: ImpactfulStory1,
       imageAlt:
         "Jucelia Pereira, ex-técnica de enfermagem que transformou sua vida ao fundar uma loja de cosméticos, sentada em um balanço cercada por folhagens.",
@@ -16,7 +20,9 @@ export const ImpactfulStories = () => {
     {
       title: "A Força que Inspira: Jornada de Fabiana Moura",
       paragraph:
-        <p>Vinda do Nordeste em busca de novas oportunidades, <span className="font-bold">Fabiana Moura</span>, 41, usou sua determinação para se especializar na área da beleza com o apoio dos cursos no Violeta Eliz. Hoje, ela é uma profissional realizada e uma líder voluntária em nosso projeto, onde usa sua poderosa história de superação para inspirar outras mulheres a transformarem seus próprios futuros.</p>,
+        <p className={paragraphClassname}>
+          Vinda do Nordeste em busca de novas oportunidades, <span className="font-bold">Fabiana Moura</span>, 41, usou sua determinação para se especializar na área da beleza com o apoio dos cursos no Violeta Eliz. Hoje, ela é uma profissional realizada e uma líder voluntária em nosso projeto, onde usa sua poderosa história de superação para inspirar outras mulheres a transformarem seus próprios futuros.
+        </p>,
       image: ImpactfulStory2,
       imageAlt:
         "Fabiana Moura, profissional da área da beleza e voluntária do projeto, sorrindo enquanto exibe brincos artesanais, em um ambiente de exposição com roupas ao fundo.",
@@ -24,33 +30,40 @@ export const ImpactfulStories = () => {
     {
       title: "Beleza na Era Digital: A História de Kauanny Soares",
       paragraph:
-        <p>Aluna da primeira turma de Empreendedorismo da ONG Violeta Eliz, <span className="font-bold">Kauanny Soares</span>, 21, aplicou o conhecimento em Marketing Digital para se lançar como Lash e Designer de Sobrancelhas. Agora, ela retribui como voluntária, usando seu talento para impulsionar o projeto nas redes sociais.</p>,
+        <p className={paragraphClassname}>
+          Aluna da primeira turma de Empreendedorismo da ONG Violeta Eliz, <span className="font-bold">Kauanny Soares</span>, 21, aplicou o conhecimento em Marketing Digital para se lançar como Lash e Designer de Sobrancelhas. Agora, ela retribui como voluntária, usando seu talento para impulsionar o projeto nas redes sociais.
+        </p>,
       image: ImpactfulStory3,
       imageAlt: "Kauanny Soares, jovem, com tiara, segurando uma pinça de design de sobrancelhas, simbolizando sua atuação como lash e designer",
     },
   ];
 
   return (
-    <section>
-      <p className='text-grey-500 text-lg'>
-        Histórias
-      </p>
-      <h2 className='text-primary-800 text-[2.5rem] font-bold'>
-        A prova do nosso trabalho
-      </h2>
-      {impactfulStories.map((story, index) =>
-        <div key={index}
-          className={`flex gap-10 h-[368px] items-center ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'}`}>
-          <Image src={story.image}
-            alt={story.imageAlt} />
-          <span className='flex flex-col gap-4 text-grey-600'>
-            <h3 className='text-primary-600 text-2xl leading-[1.875rem] font-semibold'>
-              {story.title}
-            </h3>
-            {story.paragraph}
-          </span>
-        </div>
-      )}
+    <section className="h-fit flex flex-col px-4 py-10 gap-5 tablet:px-8 tablet:py-16 tablet:gap-12 desktop:px-[4.5rem] desktop:py-16">
+      <span className="flex flex-col w-full items-start">
+        <p className='uppercase text-grey-500 text-lg font-semibold'>
+          Histórias
+        </p>
+        <h2 className='text-primary-800 text-[2.5rem] font-bold'>
+          A prova do nosso trabalho
+        </h2>
+      </span>
+      <span className="flex flex-col w-full gap-5">
+        {impactfulStories.map((story, index) =>
+          <div key={index}
+            className={`flex flex-col gap-10 h-fit items-center ${index % 2 === 0 ? 'tablet:flex-row' : 'tablet:flex-row-reverse'}`}>
+            <Image src={story.image}
+              alt={story.imageAlt}
+              className="w-auto h-[239px] tablet:h-[230px] desktop:h-[368px]" />
+            <span className='flex flex-col gap-4 text-grey-600'>
+              <h3 className='text-primary-600 text-2xl leading-6 tablet:leading-[1.875rem] desktop:leading-9 font-semibold'>
+                {story.title}
+              </h3>
+              {story.paragraph}
+            </span>
+          </div>
+        )}
+      </span>
     </section>
   )
 }
