d7e6a59f6370eaa66c0e44c671cb8cd32099c739
Dalila Castro <dalilaccastro@gmail.com>
2025-08-12 20:11:19 -0300

    refactor: add new button variant outlined-primary-700

--- src/types/button-variants.ts
+++ src/types/button-variants.ts
@@ -4,6 +4,7 @@ export const buttonVariants = {
   'text': 'bg-grey-100 text-gray-400 hover:bg-gray-100 active:bg-gray-300 focus:outline-grey-400',
   'text-grey-300': 'text-grey-500 hover:bg-grey-200 active:bg-grey-300 focus:outline-grey-400',
   'outlined-primary-400': 'bg-grey-100 border-2 border-primary-400 hover:bg-primary-200 focus:outline-primary-800 active:bg-primary-400',
-  'outlined-primary-600': 'text-primary-600 border-2 border-primary-600 hover:bg-primary-600 hover:text-grey-100'
+  'outlined-primary-600': 'text-primary-600 border-2 border-primary-600 hover:bg-primary-600 hover:text-grey-100',
+  'outlined-primary-700': 'text-primary-700 border-2 border-primary-600 hover:bg-primary-700 hover:text-grey-100',
 }
 export type ButtonVariants = keyof typeof buttonVariants
