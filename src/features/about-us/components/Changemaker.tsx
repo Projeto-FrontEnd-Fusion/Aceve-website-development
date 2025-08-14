3f856684b05f87028248b8e0fad8172077328c26
elen <elenndev@gmail.com>
2025-08-14 13:58:36 -0300

    Revertendo os dois últimos commits

--- src/features/about-us/components/Changemaker.tsx
+++ src/features/about-us/components/Changemaker.tsx
@@ -5,37 +5,32 @@ import floralFrameImage from "@/assets/changemaker.png";
 export const Changemaker = () => {
   return (
     <section className="w-full bg-primary-200 px-4 py-8 tablet:px-8 desktop:px-16">
-      <div className="grid grid-rows-[auto_auto] grid-cols-1 min-md:gap-8 ">
-        
-        <div className="row-start-2 desktop:row-start-1 flex flex-col items-center desktop:items-start min-md:gap-6">
+      <div className="grid grid-cols-1 gap-6 desktop:grid-cols-2 desktop:items-center">
+        <div className="grid desktop:grid-cols-2 gap-6 text-center desktop:text-left">
+          <h2 className="row-start-1 font-inter font-bold text-2xl tablet:text-[2rem] desktop:text-[2.5rem] leading-tight tracking-tight text-center desktop:text-left order-1">
+            <span className="block tablet:inline text-primary-600">
+              Seja um agente da{" "}
+            </span>
+            <span className="block tablet:inline text-primary-800">
+              MUDANÇA
+            </span>
+          </h2>
 
-          <div>
-            <h2 className="font-inter font-bold text-2xl tablet:text-[2rem] desktop:text-[2.5rem] leading-tight tracking-tight text-center desktop:text-left">
-              <span className="block tablet:inline text-primary-600">
-                Seja um agente da{" "}
-              </span>
-              <span className="block tablet:inline text-primary-800">
-                MUDANÇA
-              </span>
-            </h2>
-          </div>
-          <div className="row-start-1 desktop:row-start-1 flex justify-center desktop:justify-end">
+          <Image
+            src={floralFrameImage}
+            alt="Imagem institucional decorada com moldura floral"
+            className="object-cover h-auto max-w-full transition-all duration-500 row-start-2 desktop:row-start-1 desktop:col-start-2 desktop:row-span-4"
+          />
 
-            <Image
-              src={floralFrameImage}
-              alt="Imagem institucional decorada com moldura floral"
-              className="object-contain h-auto max-w-[18rem] tablet:max-w-[32rem] desktop:max-w-[50rem] transition-all duration-500"
-            />
-          </div>
-          <div className="flex flex-col gap-8 items-center">
-            <p className="font-inter text-base tablet:text-lg desktop:text-xl leading-relaxed text-grey-600 min-md:text-center desktop:text-left max-w-full">
-              Acreditamos no poder da comunidade. Cada doação garante um prato de
-              comida, um curso de tecnologia ou o suporte para uma mulher
-              recomeçar. Cada hora de voluntariado se transforma em conhecimento e
-              esperança. Sua ajuda, seja como for, é o que mantém nossa revolução
-              de amor em movimento.
-            </p>
+          <p className="desktop:col-start-1 font-inter text-base tablet:text-lg desktop:text-xl leading-relaxed text-grey-600 text-left max-w-full order-3 desktop:order-1">
+            Acreditamos no poder da comunidade. Cada doação garante um prato de
+            comida, um curso de tecnologia ou o suporte para uma mulher
+            recomeçar. Cada hora de voluntariado se transforma em conhecimento e
+            esperança. Sua ajuda, seja como for, é o que mantém nossa revolução
+            de amor em movimento.
+          </p>
 
+          <div className="desktop:col-start-1 w-full flex justify-center tablet:justify-center desktop:justify-start max-w-full order-4">
             <GlobalLink
               href="/quero-doar"
               variant="primary"
@@ -44,9 +39,7 @@ export const Changemaker = () => {
               Quero doar
             </GlobalLink>
           </div>
-
         </div>
-
       </div>
     </section>
   );
