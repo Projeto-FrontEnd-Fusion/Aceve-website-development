00e3e1d835db27bbee513caf03abb79f1276d352
Davi Santanna <davicalvalcanti@gmail.com>
2025-08-22 19:18:49 -0300

    feat: add new section ServicesWeOffer

--- src/app/o-que-fazemos/page.tsx
+++ src/app/o-que-fazemos/page.tsx
@@ -1,9 +1,11 @@
 import JoinUsSection from "@/features/what-we-do/components/JoinUsSection";
 import { ProjectsDisplay } from "../../features/what-we-do/components/ProjectDisplay";
+import { ServicesWeOffer } from "@/features/what-we-do/components/ServicesWeOffer";
 
 function WhatWeDo() {
   return (
     <main>
+      <ServicesWeOffer />
       <ProjectsDisplay />
       <JoinUsSection />
     </main>
