8909072ed6cbd428297aa8894a860376401b7f80
elen <elenndev@gmail.com>
2025-09-28 20:33:36 -0300

    test: create unit tests for volunteer phone validation

--- src/features/volunteers/__tests__/VolunteerValidation.test.ts
+++ src/features/volunteers/__tests__/VolunteerValidation.test.ts
@@ -1,13 +1,13 @@
 import { VolunteerValidation } from "../classes/VolunteerValidation"
 
-describe("Volunteer email validation", () => {
-  const validator = new VolunteerValidation();
+const validator = new VolunteerValidation();
 
-  // email
+describe("Volunteer email validation", () => {
   const emailTestFalseValues = [
-    "  ",
-    "@mail.com",
-    "user@.com"
+    "  ", // blank
+    "@mail.com", // no value before @domain.com
+    "user@.com", // no domain
+    "user.domain.com" // no @
   ]
   const emailTestTrueValues = [
     "user@gmail.com",
@@ -23,4 +23,28 @@ describe("Volunteer email validation", () => {
     expect(validator.validateEmail(email)).toBeTruthy();
   })
 
+})
+
+describe("Volunteer phone validation", () => {
+  const phoneTestFalseValues = [
+    "    ", // blank
+    "+55 (91) 9 0000-0000", // same number in xxx-xxx
+    "+55 (91) 9 2222-2222", // same number in xxx-xxx
+    "+55 (91) 8 0000-0000", // other number on the prefix istead of 9
+    "+99 (77) 9 8956-5852", // other country code
+    "+55 64 9 8956-5852", // no () in the area code
+    "55 (64) 9 2587-9514", // no + in the country code 
+    "9 5647-8954", // no country code and no area code
+  ]
+  const phoneTestTrueValues = [
+    "+55 (11) 9 1234-4432",
+    "+55 (64) 9 3355-4432",
+  ]
+
+  test.each(phoneTestFalseValues)("should return false for incorrect format phone", (phone) => {
+    expect(validator.validatePhone(phone)).toBeFalsy();
+  })
+  test.each(phoneTestTrueValues)("should return true for correct format phone", (phone) => {
+    expect(validator.validatePhone(phone)).toBeTruthy();
+  })
 })
--- src/features/volunteers/classes/VolunteerValidation.ts
+++ src/features/volunteers/classes/VolunteerValidation.ts
@@ -1,6 +1,9 @@
 export class VolunteerValidation {
+  // check the ./__tests__/VolunteerValidation.test.ts
+  // to see which vales pass and dont pass these regex
   private emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  private phoneRegex = /^\+\d{1,3}\s\(\d{2}\)\s\d\s\d{4}-\d{4}$/  // +99 (99) 9 9999-9999
+  private phoneRegex = /^\+55\s\(\d{2}\)\s\d\s(?!(\d)\1{3}-\1{4})\d{4}-\d{4}$/
+
   validateEmail(email: string): boolean {
     return this.emailRegex.test(email);
   }
