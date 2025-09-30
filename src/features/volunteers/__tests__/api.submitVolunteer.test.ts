e6a4e13b6554930ce66aa6bc63acc1640895e2e8
elen <elenndev@gmail.com>
2025-09-30 18:47:06 -0300

    test: test volunteer api route, now testing the sending email feature

--- src/features/volunteers/__tests__/api.submitVolunteer.test.ts
+++ src/features/volunteers/__tests__/api.submitVolunteer.test.ts
@@ -2,6 +2,7 @@
  * @jest-environment node
  */
 import { POST } from '../../../app/api/submit-volunteer/route'
+import nodemailer from 'nodemailer'
 
 const requestForTest = <T>(bodyContent?: T) => new Request("http://localhost:3000/api/submit-volunteer", {
   method: 'POST',
@@ -12,22 +13,55 @@ const requestForTest = <T>(bodyContent?: T) => new Request("http://localhost:300
 })
 
 //Mocks
-jest.mock('nodemailer', () => ({
-  createTransport: jest.fn().mockReturnValue({
-    sendMail: jest.fn().mockResolvedValue({
-      messageId: 'test-message-id',
-      response: '250 OK'
+// mock nodemailer
+jest.mock('nodemailer', () => {
+  const mockSendMail = jest.fn();
+  return {
+    createTransport: jest.fn().mockReturnValue({
+      sendMail: mockSendMail
     })
-  })
-}));
+  };
+});
+const mockedNodemailer = nodemailer as jest.Mocked<typeof nodemailer>;
 
-process.env.EMAIL_USER = 'test@mail.com';
-process.env.EMAIL_PASS = 'test-pass';
-
-
+// mock nodemailer method sendMail
+const mockSendMail = mockedNodemailer.createTransport().sendMail as jest.Mock;
 
 //  npm test -- -t "submit volunteer route api" --verbose
 describe("submit volunteer route api", () => {
+  beforeEach(() => {
+    process.env.EMAIL_USER = 'test@mail.com';
+    process.env.EMAIL_PASS = 'test-pass';
+
+    mockSendMail.mockReset();
+    mockSendMail.mockResolvedValue({
+      messageId: 'test-message-id',
+      response: '250 OK'
+    });
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should return status 500 when env variables are missing", async () => {
+    delete process.env.EMAIL_USER;
+    delete process.env.EMAIL_PASS;
+
+    const request = requestForTest({
+      volunteer: {
+        fullName: "João Silva",
+        email: "joao@email.com",
+        phone: "11999999999"
+      }
+    });
+    const response = await POST(request);
+    const data = await response.json();
+
+    expect(response.status).toBe(500);
+    expect(data.error).toBe("ENV variables not found");
+  });
+
+
   test("should return status 400 and message 'Request Body cannot be empty' on empty body", async () => {
     const request = requestForTest();
     const response = await POST(request);
@@ -37,13 +71,13 @@ describe("submit volunteer route api", () => {
     expect(data.error).toBe("Request Body cannot be empty");
   })
 
+
   const requestMissingFields = [
     { email: "maria@email.com", phone: "11999999999" }, // missing full name
     { fullName: "Maria Silva", phone: "11999999999" }, // missing email
     { fullName: "Maria Silva", email: "maria@email.com" }, // missing phone
     {}, // missing all fields
   ];
-
   test.each(requestMissingFields)(
     "should return status 400 and message 'Missing required fields' on incomplete volunteer information",
     async (volunteer) => {
@@ -56,5 +90,85 @@ describe("submit volunteer route api", () => {
     }
   );
 
+
+  test("should return status 400 and message 'Email with invalid format' on invalid email", async () => {
+    const request = requestForTest({
+      volunteer: {
+        fullName: "João Silva",
+        email: "@mail.com",
+        phone: "11999999999"
+      }
+    });
+    const response = await POST(request);
+    const data = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(data.error).toBe("Email with invalid format");
+  });
+
+
+  test("should return status 400 and message 'Phone with invalid format' on invalid phone", async () => {
+    const request = requestForTest({
+      volunteer: {
+        fullName: "João Silva",
+        email: "joao@email.com",
+        phone: "123"
+      }
+    });
+    const response = await POST(request);
+    const data = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(data.error).toBe("Phone with invalid format");
+  });
+
+
+  test("should return status 502 and message 'Error trying to send email' when email to ONG is rejected", async () => {
+    mockSendMail.mockResolvedValueOnce({
+      rejected: ['recipient@email.com'], // oNG email rejected
+      messageId: 'message-1'
+    });
+
+    const request = requestForTest({
+      volunteer: {
+        fullName: "João Silva",
+        email: "joao@gmail.com",
+        phone: "+55 (11) 9 1234-4432",
+        message: "Quero ser voluntário"
+      }
+    });
+    const response = await POST(request);
+    const data = await response.json();
+
+    expect(response.status).toBe(502);
+    expect(data.error).toBe("Error trying to send email");
+  });
+
+
+  test("should return status 502 and message 'Error trying to send confirm email to volunteer' when confirmation email to volunteer is rejected", async () => {
+    mockSendMail
+      .mockResolvedValueOnce({
+        rejected: [], // ONG email accepcted 
+        messageId: 'message-1'
+      })
+      .mockResolvedValueOnce({
+        rejected: ['volunteer@email.com'], // volunteer email rejected
+        messageId: 'message-2'
+      });
+
+    const request = requestForTest({
+      volunteer: {
+        fullName: "João Silva",
+        email: "joao@email.com",
+        phone: "+55 (11) 9 1234-4432",
+        message: "Quero ser voluntário"
+      }
+    });
+    const response = await POST(request);
+    const data = await response.json();
+
+    expect(response.status).toBe(502);
+    expect(data.error).toBe("Error trying to send confirm email to volunteer");
+  });
 
 })
