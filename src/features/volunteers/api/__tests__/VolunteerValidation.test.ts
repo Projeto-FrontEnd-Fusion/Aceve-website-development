d42059e969449d4e625af475349487e6a4ab94cf
elen <elenndev@gmail.com>
2025-10-03 15:53:48 -0300

    update VolunteerValidation.test to use zod validator

--- src/features/volunteers/api/__tests__/VolunteerValidation.test.ts
+++ src/features/volunteers/api/__tests__/VolunteerValidation.test.ts
@@ -1,8 +1,6 @@
-import { VolunteerValidation } from "../classes/VolunteerValidation"
+import { volunteerFormValidator } from "../../utils/volunteerFormValidator";
 
-const validator = new VolunteerValidation();
-
-// npm test -- -t "Volunteer phone validation" --verbose
+// npm test -- -t "Volunteer email validation" --verbose
 describe("Volunteer email validation", () => {
   const emailTestFalseValues = [
     "  ", // blank
@@ -17,35 +15,63 @@ describe("Volunteer email validation", () => {
   ]
 
   test.each(emailTestFalseValues)("should return false for incorrect format email", (email) => {
-    expect(validator.validateEmail(email)).toBeFalsy();
+    const validation = volunteerFormValidator.safeParse({
+      name: "Ana Santos",
+      phoneNumber: "91900000000",
+      email,
+      description: "Quero ser voluntária"
+    })
+    expect(validation.error).toBeTruthy();
+
   })
 
   test.each(emailTestTrueValues)("should return true for correct format email", (email) => {
-    expect(validator.validateEmail(email)).toBeTruthy();
+    const validation = volunteerFormValidator.safeParse({
+      name: "Ana Santos",
+      phoneNumber: "91900000000",
+      email,
+      description: "Quero ser voluntária"
+    })
+
+    expect(validation.success).toBeTruthy();
   })
 
 })
 
+// npm test -- -t "Volunteer phone validation" --verbose
 describe("Volunteer phone validation", () => {
   const phoneTestFalseValues = [
-    "    ", // blank
-    "+55 (91) 9 0000-0000", // same number in xxx-xxx
-    "+55 (91) 9 2222-2222", // same number in xxx-xxx
-    "+55 (91) 8 0000-0000", // other number on the prefix istead of 9
-    "+99 (77) 9 8956-5852", // other country code
-    "+55 64 9 8956-5852", // no () in the area code
-    "55 (64) 9 2587-9514", // no + in the country code 
-    "9 5647-8954", // no country code and no area code
+    "    ",
+    // (91) 9 0000-00000
+    "918000000000", // 12 caracteres
+    // 9 5647-8954
+    "956478954", // no area code
   ]
   const phoneTestTrueValues = [
-    "+55 (11) 9 1234-4432",
-    "+55 (64) 9 3355-4432",
+    "11912344432", // (11) 9 1234-4432
+    "64933554432", // (64) 9 3355-4432
   ]
 
-  test.each(phoneTestFalseValues)("should return false for incorrect format phone", (phone) => {
-    expect(validator.validatePhone(phone)).toBeFalsy();
+  test.each(phoneTestFalseValues)("should return false for incorrect format phone", (phoneNumber) => {
+    const validation = volunteerFormValidator.safeParse({
+      name: "Ana Santos",
+      phoneNumber,
+      email: "user@gmail.com",
+      description: "Quero ser voluntária"
+    })
+
+
+    expect(validation.error).toBeTruthy();
   })
-  test.each(phoneTestTrueValues)("should return true for correct format phone", (phone) => {
-    expect(validator.validatePhone(phone)).toBeTruthy();
+  test.each(phoneTestTrueValues)("should return true for correct format phone", (phoneNumber) => {
+    const validation = volunteerFormValidator.safeParse({
+      name: "Ana Santos",
+      phoneNumber,
+      email: "user@gmail.com",
+      description: "Quero ser voluntária"
+    })
+
+
+    expect(validation.success).toBeTruthy();
   })
 })
