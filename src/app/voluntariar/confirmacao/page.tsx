80ecf6db2f0e121bdfe668e006d4544031470c24
Caroline Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2025-10-03 19:05:38 +0100

    refactor: add fine adjustments on Confimation message

--- src/app/voluntariar/confirmacao/page.tsx
+++ src/app/voluntariar/confirmacao/page.tsx
@@ -1,9 +1,9 @@
 export default function ConfirmationPage() {
   return (
     <section className="flex items-center justify-center min-h-screen bg-purple-100 p-4">
-      <div className="w-[220px] h-[220px] md:w-[240px] md:h-[240px]  bg-gray-100 rounded-[16px] border border-purple-600 flex flex-col items-center justify-center p-4 text-center">
+      <div className="w-[200px] h-[200px] md:w-[250px] md:h-[250px]  bg-gray-100 rounded-[16px] border border-purple-600 flex flex-col items-center justify-center p-4 text-center">
         <h1 className="text-base md:text-lg font-bold text-purple-700 mb-6">
-          Obrigado por se inscrever como voluntário!
+          Obrigada por se inscrever como voluntário!
         </h1>
         <p className="text-xs md:text-sm text-purple-500">
           Você receberá um e-mail de confirmação em breve.
