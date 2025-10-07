e9a868043e220dc944ec28397c3db9ca37119e02
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-10-07 16:09:14 -0300

    refactor: improve typesetting and replace forwardRef with useFormContext
    - The typesetting was improved, now allowing typescript to determine
    what attributes are allowed or not depending on the `as` attribute
    (textarea vs input)
    
    - forwardRef was replaced with `FormProvided` and `useFormContext()`
    hooks

--- src/components/Inputs/Inputs.tsx
+++ src/components/Inputs/Inputs.tsx
@@ -1,36 +1,62 @@
-import { forwardRef } from "react";
-import { FieldError } from "react-hook-form";
+import {
+  HTMLInputTypeAttribute,
+  InputHTMLAttributes,
+  TextareaHTMLAttributes
+} from 'react'
+import { FieldError, useFormContext } from 'react-hook-form'
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement> & React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
-    error?: FieldError;
-    type?: string;
-    height?: number;
+interface Props {
+  name: string
+  error?: FieldError
+  mask?: (value: string) => string
+}
 
-};
+interface InputFieldProps
+  extends Props,
+    Omit<InputHTMLAttributes<HTMLInputElement>, 'name'> {
+  as?: 'input'
+  type: HTMLInputTypeAttribute
+  height?: never
+}
 
-export const Inputs = forwardRef<HTMLInputElement | HTMLTextAreaElement, InputProps>(({ type = "text", error, height, ...rest }: InputProps, ref) => {
-    return (
-        <div className="mb-4 min-md:mb-8">
-            {type === "textarea" ? (
-                <textarea
-                    ref={ref as React.Ref<HTMLTextAreaElement>}
-                    className={`w-full border-primary-500 border-2 rounded-lg py-4 px-2 min-md:p-4 text-grey-500 text-base placeholder:text-grey-400 resize-none ${error ? "border-red-500" : ""
-                        }`}
-                    style={{ height: height ? `${height}px` : "128px" }}
-                    {...rest}
-                />
-            ) : (
-                <input
-                    type={type}
-                    ref={ref as React.Ref<HTMLInputElement>}
-                    className={`w-full border-primary-500 border-2 rounded-lg py-4 px-2 min-md:p-4 text-grey-500 text-base placeholder:text-grey-400 ${error ? "border-red-500" : ""
-                        }`}
-                    {...rest}
-                />
-            )}
-            {error && (
-                <p className="text-red-500 text-sm mt-1">{error.message?.toString()}</p>
-            )}
-        </div>
-    )
-})
+interface TextAreaFieldProps
+  extends Props,
+    Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'name'> {
+  as: 'textarea'
+  height?: number
+  type?: never
+}
+
+type InputProps = InputFieldProps | TextAreaFieldProps
+
+export const Inputs = (props: InputProps) => {
+  const { register } = useFormContext()
+
+
+  const inputElement = props.as === 'textarea' ? (
+    <textarea
+      className={`w-full border-primary-500 border-2 rounded-lg py-4 px-2 min-md:p-4 text-grey-500 text-base placeholder:text-grey-400 resize-none ${error ? 'border-red-500' : ''}`}
+      style={{ height: props.height ? `${props.height}px` : '128px' }}
+      {...register(name)}
+      {...(props as TextAreaFieldProps)}
+    />
+  ) : (
+    <input
+      className={`w-full border-primary-500 border-2 rounded-lg py-4 px-2 min-md:p-4 text-grey-500 text-base placeholder:text-grey-400 ${error ? 'border-red-500' : ''}`}
+      {...register(name)}
+      {...(props as InputFieldProps)}
+    />
+  )
+
+  return (
+    <div className='mb-4 min-md:mb-8'>
+      {inputElement}
+      {error && (
+        <p className='text-red-500 text-sm mt-1'>
+          {error.message?.toString()}
+        </p>
+      )}
+    </div>
+  )
+}
+
--- src/features/volunteers/components/VolunteerForm/VolunteerForm.tsx
+++ src/features/volunteers/components/VolunteerForm/VolunteerForm.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 import { Inputs } from "@/components/Inputs/Inputs";
-import { zodResolver } from "@hookform/resolvers/zod"
+import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js'
 import { volunteerValidator } from "../../utils/volunteerValidator";
+
 type FormData = {
   name: string;
   phoneNumber: string;
@@ -12,52 +13,54 @@ type FormData = {
 
 
 export const VolunteerForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(volunteerValidator) });
+    const methods = useForm<FormData>({ resolver: zodResolver(volunteerValidator) })
+    const { formState: { errors } } = methods
 
+    const onSubmit = async (data: FormData) => {
+      console.log("Form data:", data);
+    };
 
-  const onSubmit = (data: FormData) => {
-    console.log("Form data:", data);
-  };
+    return (
+        <FormProvider {...methods}>
+          <form className="w-full h-full m-auto py-10 px-6 min-md:px-10 bg-white-normal rounded-2xl border-primary-500 border-2 
+          flex flex-col justify-between" 
+            onSubmit={methods.handleSubmit(onSubmit)}>
+              <Inputs
+                  name="name"
+                  type="text"
+                  placeholder="Nome completo"
+                  id="name"
+                  error={errors.name}
+              />
 
-  return (
-    <form className="w-full h-full m-auto py-10 px-6 min-md:px-10 bg-white-normal rounded-2xl border-primary-500 border-2 
-        flex flex-col justify-between"
-      onSubmit={handleSubmit(onSubmit)}>
-      <Inputs
-        {...register("name")}
-        type="string"
-        placeholder="Nome completo"
-        id="name"
-        error={errors.name}
-      />
+              <Inputs
+                  name="phoneNumber"
+                  type="tel"
+                  placeholder="Telefone"
+                  id="phoneNumber"
+                  error={errors.phoneNumber}
+              />
 
-      <Inputs
-        {...register("phoneNumber")}
-        type="tel"
-        placeholder="Telefone"
-        id="phoneNumber"
-        error={errors.phoneNumber}
-      />
+              <Inputs
+                  name="email"
+                  type="email"
+                  placeholder="E-mail"
+                  id="email"
+                  error={errors.email}
+              />
 
-      <Inputs
-        {...register("email")}
-        type="email"
-        placeholder="E-mail"
-        id="email"
-        error={errors.email}
-      />
-
-      <Inputs
-        {...register("description")}
-        type="textarea"
-        placeholder="Como pode contribuir com a ONG"
-        id="description"
-        error={errors.description}
-        height={240}
-      />
-      <button className="w-full border-2 rounded-lg p-4 bg-primary-600 text-xl font-semibold text-grey-100" type="submit">
-        Enviar Formulário
-      </button>
-    </form>
-  )
+              <Inputs
+                  name="description"
+                  as="textarea"
+                  placeholder="Como pode contribuir com a ONG"
+                  id="description"
+                  error={errors.description}
+                  height={240}
+              />
+              <button className="w-full border-2 rounded-lg p-4 bg-primary-600 text-xl font-semibold text-grey-100" type="submit">
+                  Enviar Formulário
+              </button>
+          </form>
+        </FormProvider>
+    )
 };
