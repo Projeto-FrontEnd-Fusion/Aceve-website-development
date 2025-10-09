6910ae6ad4fef0978f533f4f3caceb8e9bac5d48
elen <elendev@gmail.com>
2025-10-09 14:13:48 -0300

    feat: change validation messages

--- src/features/volunteers/utils/volunteerValidator.ts
+++ src/features/volunteers/utils/volunteerValidator.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const volunteerValidator = z.object({
   name: z
     .string()
-    .min(2, { message: "O nome deve ter pelo menos 2 caracteres" }),
+    .min(2, { message: "O nome é obrigatório" }),
   phoneNumber: z
     .string()
     .min(15, { message: "O telefone deve ter 11 digitos" })
@@ -11,5 +11,5 @@ export const volunteerValidator = z.object({
   email: z
     .email("Fornaça um e-mail válido")
     .min(1, { message: "O email é obrigatório" }),
-  description: z.string().min(5, { message: "A mensagem é obrigatória" }),
+  description: z.string().min(5, { message: "A mensagem é muito curta" })
 });
