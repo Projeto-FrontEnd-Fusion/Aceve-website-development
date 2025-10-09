24b52ffac811f5754a192da5bdfa71d8680b99e5
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-09 00:44:32 -0300

    feat: add API submission and loading state, and use IVolunteer type instead of FormData

--- src/features/volunteers/components/VolunteerForm/VolunteerForm.tsx
+++ src/features/volunteers/components/VolunteerForm/VolunteerForm.tsx
@@ -1,55 +1,80 @@
 "use client";
-import { FormProvider, useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form";
 import { InputProps, Inputs } from "@/components/Inputs/Inputs";
-import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js'
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 import { volunteerValidator } from "../../utils/volunteerValidator";
 import { phoneMask } from "../../utils/phoneMask";
 import { GlobalButton } from "@/components/GlobalButton/GlobalButton";
-
-type FormData = {
-  name: string;
-  phoneNumber: string;
-  email: string;
-  description: string
-}
-
+import { submitVolunteer } from "../../services/submitVolunteer";
+import { type IVolunteer } from "../../types/IVolunteer";
+import { useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const VolunteerForm = () => {
+  const [Isloading, setIsLoading] = useState<boolean>(false);
 
-    const methods = useForm<FormData>({ resolver: zodResolver(volunteerValidator) })
-    const { formState: { errors } } = methods
+  const methods = useForm<IVolunteer>({
+    resolver: zodResolver(volunteerValidator),
+  });
+  const {
+    formState: { errors },
+  } = methods;
 
-    const onSubmit = async (data: FormData) => {
-      console.log("Form data:", data);
-    };
+  const onSubmit = (data: IVolunteer) => {
+    setIsLoading(true);
+    submitVolunteer(data, (succeeded) =>
+      succeeded ? setIsLoading(false) : alert("Falha ao enviar o formulário")
+    );
+  };
 
-    const fields: readonly InputProps[] = [
-      { name: "name", type: "text", placeholder: "Nome completo" },
-      { name: "phoneNumber", type: "tel", placeholder: "Telefone", mask: phoneMask },
-      { name: "email", type: "email", placeholder: "E-mail" },
-      { name: "description", as: "textarea", placeholder: "Como pode contribuir com a ONG", height: 240 },
-    ];
+  const fields: readonly InputProps[] = [
+    { name: "name", type: "text", placeholder: "Nome completo" },
+    {
+      name: "phoneNumber",
+      type: "tel",
+      placeholder: "Telefone",
+      mask: phoneMask,
+    },
+    { name: "email", type: "email", placeholder: "E-mail" },
+    {
+      name: "description",
+      as: "textarea",
+      placeholder: "Como pode contribuir com a ONG",
+      height: 240,
+    },
+  ];
 
-
-    return (
-        <FormProvider {...methods}>
-          <form className="w-full h-full m-auto py-10 px-6 min-md:px-10 bg-white-normal rounded-2xl border-primary-500 border-2 
-          flex flex-col justify-between" 
-            onSubmit={methods.handleSubmit(onSubmit)}>
-              {fields.map((field) => (
-                <Inputs
-                  key={field.name}
-                  {...field}
-                  id={field.name}
-                  error={errors[field.name as keyof FormData]}
-                />
-              ))}
-              <GlobalButton
-                type="submit"
-                variant="primary"
-                className="p-4 text-xl font-semibold"
-                >Enviar Formulário</GlobalButton>
-          </form>
-        </FormProvider>
-    )
+  return (
+    <FormProvider {...methods}>
+      <form
+        className="w-full h-full m-auto py-10 px-6 min-md:px-10 bg-white-normal rounded-2xl border-primary-500 border-2 
+          flex flex-col justify-between"
+        onSubmit={methods.handleSubmit(onSubmit)}
+      >
+        {fields.map((field) => (
+          <Inputs
+            key={field.name}
+            {...field}
+            id={field.name}
+            error={errors[field.name as keyof IVolunteer]}
+          />
+        ))}
+        <GlobalButton
+          type="submit"
+          disabled={Isloading}
+          variant={Isloading ? "disabled" : "primary"}
+          className="p-4 text-xl font-semibold relative"
+        >
+          {Isloading ? (
+            <>
+              Enviando...
+              <AiOutlineLoading3Quarters className="animate-spin  absolute right-6" />
+            </>
+          ) : (
+            "Enviar Formulário"
+          )}
+        </GlobalButton>
+      </form>
+    </FormProvider>
+  );
 };
