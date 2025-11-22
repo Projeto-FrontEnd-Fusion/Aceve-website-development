3101974fa688c3d5a3b746103bee2d5ec8b54fbb
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-11-22 11:15:10 -0300

    feat: change 'Parceiros' to 'Apoiadores' due to client's request

--- src/features/home/components/Partners/OurPartners.tsx
+++ src/features/home/components/Partners/OurPartners.tsx
@@ -19,7 +19,7 @@ export const OurPartners = () => {
   return (
     <section className="py-12 flex flex-col gap-8 bg-primary-100">
       <h2 className="max-mobileLarger:text-lg text-3xl font-bold text-primary-800 uppercase self-center">
-        parceiros
+         Apoiadores
       </h2>
       <div className="flex justify-center gap-8 items-center max-mobileLarger:flex-col md:gap-20">
         {patnesImg.map(({ name, icon }) => (
