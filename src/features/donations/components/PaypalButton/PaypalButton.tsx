9ef1f23add67d82f6e14001f3fd285e7f2d0d004
elen <elenndev@gmail.com>
2025-12-07 00:35:40 -0300

    feat: show error message

--- src/features/donations/components/PaypalButton/PaypalButton.tsx
+++ src/features/donations/components/PaypalButton/PaypalButton.tsx
@@ -1,10 +1,12 @@
-'use client'
-
 import { usePaypal } from '@/features/donations/hooks/usePaypal'
 import { useDonationStore } from '@/features/donations/stores/donationvalue.store'
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js'
 
-export const PaypalButton = () => {
+interface PaypalButtonProps {
+  onErrorChange: (hasError: boolean) => void,
+  cleanPaypalError: () => void;
+}
+export const PaypalButton = ({ onErrorChange, cleanPaypalError }: PaypalButtonProps) => {
   const donationValue = useDonationStore((state) => state.donationValue)
   const { options, requestCreateOrder, requestOrderCapture } = usePaypal()
 
@@ -20,8 +22,11 @@ export const PaypalButton = () => {
           }}
           className="flex z-10 h-[56px] bg-[#EEEEEE] hover:bg-[#E2E2E2] rounded-md justify-start items-center"
           createOrder={requestCreateOrder(donationValue)}
+          onClick={cleanPaypalError}
+          onError={() => onErrorChange(true)}
           onApprove={requestOrderCapture} />
       }
+
     </PayPalScriptProvider>
   )
 }
