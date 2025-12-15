382a38626e0b3d3884d692393e79e1c55d35129e
Caroline Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2025-12-15 17:14:58 +0000

    feat: Add EventForm page

---
+++ src/app/(admin)/admin/admin-events/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+import EventForm from "@/features/admin-login/components/EventForm";
+export default function EventDashboard() {
+  return <EventForm />;
+}
---
+++ src/features/admin-login/components/EventForm.tsx
@@ -0,0 +1,51 @@
+import { FormProvider, useForm } from "react-hook-form";
+
+export type EventFormData = {
+  name: string;
+  description?: string;
+  total: string;
+  beneficiaries: number;
+  day: string;
+  month: string;
+  year: string;
+};
+
+export default function EventForm() {
+  const methods = useForm<EventFormData>({
+    defaultValues: {
+      name: "",
+      description: "",
+      total: "",
+      beneficiaries: 0,
+      day: "",
+      month: "",
+      year: "",
+    },
+  });
+  function onSubmit(data: EventFormData) {
+    console.log("dados do formulário", data);
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <section className="w-full h-full bg-primary-100 rounded-lg p-8">
+        <header mb-8>
+          <a href="" className="text-sm text-grey-500 mb-4">
+            Voltar
+          </a>
+          <h1 className="text-grey-800 font-semibold text-2xl">
+            Registre seu evento
+          </h1>
+          <p className="text-sm text-black mt-1">
+            Preencha os detalhes para registrar este evento com sucesso.
+          </p>
+        </header>
+
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          className="space-y-6"
+        ></form>
+      </section>
+    </FormProvider>
+  );
+}
