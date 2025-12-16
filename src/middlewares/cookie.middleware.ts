ece80c96f31c7ad160ffd2c16e6abf4c51452c46
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2025-12-16 13:17:55 -0300

    refactor: remove unnecessary checks

--- src/middlewares/cookie.middleware.ts
+++ src/middlewares/cookie.middleware.ts
@@ -3,22 +3,19 @@ import { NextResponse } from "next/server";
 import { CustomMiddleware } from './chain.middleware';
 
 const PUBLIC_API_PATHS = [
-    "/api/auth/login",
-    "/api/auth/new-user",
+   "/api/auth/login",
 ];
 
 export function cookieAuth(next: CustomMiddleware): CustomMiddleware {
   return async (req, event, response) => {
-    const { pathname } = req.nextUrl;
-    const host = req.headers.get("host") ?? "";
 
-    const isAdminDomain = host.startsWith("admin.");
+    const { pathname } = req.nextUrl;
     const isApiRoute = pathname.startsWith("/api");
     const isPublicApi = PUBLIC_API_PATHS.some(path =>
       pathname.startsWith(path)
     );
 
-    if (isAdminDomain && isApiRoute && !isPublicApi) {
+    if (isApiRoute && !isPublicApi) {
       const hasSessionCookie = req.cookies.has("better-auth.session-token");
 
       if (!hasSessionCookie) {
