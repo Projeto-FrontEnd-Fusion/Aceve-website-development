3731e4453131dfd87d3006a6e31093ea4cc8e49b
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2025-12-18 14:40:05 -0300

    refactor: remove unnecessary method

--- src/features/admin-login/api/auth/auth.service.ts
+++ src/features/admin-login/api/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import auth from "../utils/auth";
 import { LoginRequest, LoginSchema } from "./dtos/login.dto";
-import { CreateLogin, CreateLoginSchema } from "./dtos/create.dto";
 
 export const AuthService = async () => {
 
@@ -18,21 +17,6 @@ export const AuthService = async () => {
         return NextResponse.json(await response.json(), {status: 200, headers:{"Set-Cookie": cookies!}});
     }
 
-    const Create = async (data: CreateLogin): Promise<NextResponse> => {
-        const createDto = await CreateLoginSchema.safeParseAsync(data);
-        if (!createDto.success) {
-            return NextResponse.json({ error: "Dados de solicitação inválidos!", details: createDto.error.cause }, { status: 400 });
-        }
-
-        const response = await auth.api.signUpEmail({
-            body: {...createDto.data},
-            headers: headers(),
-            asResponse: true
-        });
-
-        return NextResponse.json(await response.json(), {status: 201});
-    }
-
     const GetSession = async (request: NextRequest): Promise<NextResponse> => {
         const response = await auth.api.getSession({ headers: request.headers, asResponse: true });
         if (!response.ok) {
@@ -41,5 +25,5 @@ export const AuthService = async () => {
         return NextResponse.json(await response.json(), {status: 200});
     }
     
-    return { Login, Create, GetSession } as const;
+    return { Login, GetSession } as const;
 }
