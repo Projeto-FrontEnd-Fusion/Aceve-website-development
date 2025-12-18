edc920508c222a13fe1f9bc6b470635417d11e3e
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2025-12-18 14:33:12 -0300

    chore: remove Users model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,19 @@
 import auth from "@/features/admin-login/api/utils/auth";
 import { prisma } from "@/utils/prisma";
-
-
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// npx prisma migrate dev
 // npx prisma generate
 // npx prisma db seed
-
 async function seed() {
   const email = process.env.ADMIN_EMAIL;
   const password = process.env.ADMIN_PASSWORD;
   const name = process.env.ADMIN_NAME;
-
-
   if (!email || !password || !name) {
     throw new Error("ADMIN_EMAIL, ADMIN_PASSWORD e ADMIN_NAME precisam estar no .env");
   }
+
   const result = await auth.api.signUpEmail({
     body: {
       name,
@@ -24,8 +21,6 @@ async function seed() {
       password,
     },
   });
-
-
   if (!result?.user) {
     throw new Error("Falha ao criar usuário admin");
   }
