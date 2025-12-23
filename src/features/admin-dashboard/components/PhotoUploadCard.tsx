1ed35630ef4e78d669ca78de6eb53c65311bd0cb
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-12-23 02:31:13 -0300

    refactor: change upload card style

--- src/features/admin-dashboard/components/PhotoUploadCard.tsx
+++ src/features/admin-dashboard/components/PhotoUploadCard.tsx
@@ -1,16 +1,15 @@
 export function PhotUploadCard() {
   return (
-    <div className="relative bg-white border border-neutral-200 rounded-md p-3 flex flex-col gap-3 shadow-sm">
-      <button className="absolute top-2 right-2 text-xs text-neutral-400">
+    <div className="relative bg-white border border-neutral-200 rounded-sm p-4 flex flex-col gap-3 shadow-md">
+      <button className="absolute top-2 right-2 text-sm text-neutral-700">
         X
       </button>
-      <div className="w-full aspect-square bg-purple-200 rounded-sm">
-        <input
-          type="text"
-          placeholder="Digite uma legenda opcional... "
-          className="text-xs text-neutral-700 border-none border-b border-neutral-300 focus:outline-none focus:border-neutral-500 bg-tranparent pt-2"
-        />
-      </div>
+      <div className="w-full aspect-square bg-primary-300 rounded-sm" />
+      <input
+        type="text"
+        placeholder="Digite uma legenda opcional..."
+        className="text-sm text-grey-600 border-none focus:outline-none bg-transparent"
+      />
     </div>
   );
 }
