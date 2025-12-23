536b31a6d83908ba6ff66731869a5aad5b79aeba
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-12-23 00:20:06 -0300

    refactor: separata form and non-form content and use global Input for event form

--- src/features/admin-dashboard/components/EventForm.tsx
+++ src/features/admin-dashboard/components/EventForm.tsx
@@ -1,9 +1,7 @@
 import { FormProvider, useForm } from "react-hook-form";
-import { FormInput } from "./FormInput";
-import { FormTextarea } from "./FormTextarea";
+import { Inputs } from "@/components/Inputs/Inputs";
 import { GlobalButton } from "@/components/GlobalButton/GlobalButton";
 import { EventDate } from "./EventDate";
-import { register } from "module";
 import { PhotUploadCard } from "./PhotoUploadCard";
 
 export type EventFormData = {
@@ -28,55 +26,75 @@ export default function EventForm() {
       year: "",
     },
   });
+
   function onSubmit(data: EventFormData) {
     console.log("dados do formulário", data);
   }
 
-  return (
-    <FormProvider {...methods}>
-      <section className="bg-primary-100 rounded-lg min-h-screen w-full">
-        <div className="w-full max-w-[960px] px-8 py-10">
-          <header mb-8>
-            <a
-              href=""
-              className="text-sm text-grey-500 mb-2 inline-flex items-center gap-2"
-            >
-              Voltar
-            </a>
-            <h1 className="text-grey-800 font-semibold text-2xl">
-              Registre seu evento
-            </h1>
-            <p className="text-sm text-black mt-1">
-              Preencha os detalhes para registrar este evento com sucesso.
-            </p>
-          </header>
+  const {
+    formState: { errors },
+  } = methods;
 
+  return (
+    <section className="bg-primary-100 rounded-lg min-h-screen w-full">
+      <div className="w-full max-w-[960px] px-8 py-10">
+        <header className="mb-8">
+          <a
+            href=""
+            className="text-sm text-grey-500 mb-2 inline-flex items-center gap-2"
+          >
+            Voltar
+          </a>
+          <h1 className="text-grey-800 font-semibold text-2xl">
+            Registre seu evento
+          </h1>
+          <p className="text-sm text-black mt-1">
+            Preencha os detalhes para registrar este evento com sucesso.
+          </p>
+        </header>
+        <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-6">
-            <FormInput
+            {/* Nome do evento */}
+            <Inputs
               name="name"
-              label="Nome do Evento"
-              rules={{ required: "Nome do evento é obrigatório" }}
+              type="text"
+              placeholder="Nome do Evento"
+              error={errors.name}
             />
-            <FormTextarea
+
+            {/* Descrição */}
+            <Inputs
+              as="textarea"
               name="description"
-              label="Descrição do evento (opcional)"
+              placeholder="Descrição do evento (opcional)"
+              height={120}
+              error={errors.description}
               maxLength={500}
             />
-            <div className="grid grid cols-2 gap-6">
-              <FormInput name="total" label="Total arrecadado" />
-              <FormInput
+
+            {/* Total / Beneficiários */}
+            <div className="grid grid-cols-2 gap-6">
+              <Inputs
+                name="total"
+                type="text"
+                placeholder="Total arrecadado"
+                error={errors.total}
+              />
+
+              <Inputs
                 name="beneficiaries"
-                label="Pessoas beneficiadas"
                 type="number"
-                rules={{
-                  required: "Informe o número de pessoas",
-                  valueAsNumber: true,
-                }}
+                placeholder="Pessoas beneficiadas"
+                error={errors.beneficiaries}
               />
             </div>
+
+            {/* Data */}
             <div className="flex items-end gap-6">
               <EventDate />
             </div>
+
+            {/* Upload de fotos */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
               <PhotUploadCard />
               <PhotUploadCard />
@@ -93,8 +111,8 @@ export default function EventForm() {
               Salvar Registro
             </GlobalButton>
           </form>
-        </div>
-      </section>
-    </FormProvider>
+        </FormProvider>
+      </div>
+    </section>
   );
 }
