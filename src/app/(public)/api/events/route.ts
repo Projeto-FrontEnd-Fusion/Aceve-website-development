7fd5166d6e8cc5e14d288f50272c50dc5d112bcd
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2026-01-13 18:51:01 -0300

    fix: move route from admin to public access

--- src/app/(public)/api/events/route.ts
+++ src/app/(public)/api/events/route.ts
@@ -3,6 +3,6 @@ import { NextRequest } from "next/server";
 
 const controller = await EventController();
 
-export async function POST(request: NextRequest) {
-  return await controller.CreateEvent(request);
+export async function GET(request: NextRequest) {
+  return await controller.FindLatest(request);
 }
